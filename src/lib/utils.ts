6db11a2417e8b01290a587ec93853d7be8de99b4
secondubly <secondubly@gmail.com>
2023-10-21 10:50:03 -0400

    fix module resolution

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,24 +1,24 @@
-import type { Options } from "tmi.js"
-import dotenv from 'dotenv'
-dotenv.config({ path: '..\.env.development.local' }) // TODO: in production you can remove the path
+import type { Options } from 'tmi.js'
+import { config } from 'dotenv'
+
+config({ path: process.cwd() + '/src/.env'})
 
 export const CLIENT_OPTIONS: Options = {
-    options: {
-        debug: (process.env.NODE_ENV === 'development' ? true : false)
-    },
-    channels: envParseArray('TWITCH_CHANNELS', []),
-    identity: {
-        username: process.env.BOT_USERNAME,
-        password: process.env.BOT_OAUTH_TOKEN
-    }
+	options: {
+		debug: process.env.NODE_ENV === 'development' ? true : false
+	},
+	channels: envParseArray('TWITCH_CHANNELS', []),
+	identity: {
+		username: process.env.BOT_USERNAME,
+		password: process.env.BOT_OAUTH_TOKEN
+	}
 }
 
 export function envParseArray(key: string, defaultValue: string[]) {
-    const value = process.env[key];
-    if (!value) {
-      if (defaultValue === void 0)
-        throw new ReferenceError(`[ENV] ${key} - The key must be an array, but is empty or undefined.`);
-      return defaultValue;
-    }
-    return value.split(" ");
+	const value = process.env[key]
+	if (!value) {
+		if (defaultValue === void 0) throw new ReferenceError(`[ENV] ${key} - The key must be an array, but is empty or undefined.`)
+		return defaultValue
+	}
+	return value.split(' ')
 }
