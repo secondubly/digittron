ebddf54d24d7f30c1cc4cd281ea74eb5d24a480c
secondubly <secondubly@gmail.com>
2023-10-23 10:57:53 -0400

    todo finished (for now)

--- src/listeners/message.ts
+++ src/listeners/message.ts
@@ -3,7 +3,6 @@ import commandHandler from '../lib/commands/commandHandler'
 
 export function onMessage(channel: string, state: ChatUserstate, message: string, self: boolean) {
 	if (self) return
-	// TODO: command handler
 	if(message.trim().startsWith('!')) {
 		commandHandler.runCommand(channel, message)
 	}
