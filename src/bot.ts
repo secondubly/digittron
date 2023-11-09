9d7ccdb02eecc9e69225221027f2d7f3ce7bea47
secondubly <secondubly@gmail.com>
2023-11-09 13:45:49 -0500

    register event listeners first

--- src/bot.ts
+++ src/bot.ts
@@ -2,10 +2,10 @@ import { client } from './client'
 import { onConnect } from './listeners/connect'
 import { onMessage } from './listeners/message'
 
-await client.connect().catch(async (err) => {
-	console.error(err)
-})
-
 client.on('connected', onConnect)
 
 client.on('message', onMessage)
+
+await client.connect().catch(async (err) => {
+	console.error(err)
+})
