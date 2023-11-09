c1613c133115b7611c09aeb451677c6c03eac90b
secondubly <secondubly@gmail.com>
2023-11-09 10:25:41 -0500

    add commands from phantombot

--- src/lib/commands/commandHandler.ts
+++ src/lib/commands/commandHandler.ts
@@ -1,42 +1,41 @@
-import { client } from "../../client"
-import { isNullOrEmpty } from "../utils"
+import { client } from '../../client'
+import { isNullOrEmpty } from '../utils'
 
 class CommandHandler {
+	runCommand(channel: string, message: string) {
+		const { command, args } = this.parseCommand(message)
 
-    runCommand(channel: string, message: string) {
-        const {command, args } = this.parseCommand(message)
+		if (isNullOrEmpty(command)) {
+			return
+		}
 
-        if (isNullOrEmpty(command)) {
-            return
-        }
+		switch (command) {
+			case 'test':
+				client.say(channel, 'this is a test of the emergency bot system')
+				break
+			default:
+				break
+		}
+	}
 
-        switch (command) {
-            case 'test':
-                client.say(channel, 'this is a test of the emergency bot system')
-                break
-            default:
-                break
-        }
-    }
+	private parseCommand(message: string) {
+		const regex = /\!(.*?)$/gm //eslint-disable-line
+		const fullCommand = regex.exec(message)
 
-    private parseCommand(message: string) {
-        const regex = /\!(.*?)$/gm
-        const fullCommand = regex.exec(message)
+		if (fullCommand) {
+			// position 0 is the command delimiter
+			const splitCommand = fullCommand[1].split(' ')
+			const command = splitCommand[0]
 
-        if (fullCommand) {
-            // position 0 is the command delimiter
-            const splitCommand = fullCommand[1].split(' ')
-            const command = splitCommand[0]
-            
-            splitCommand.shift()
-            return {
-                command,
-                args: splitCommand
-            }
-        }
+			splitCommand.shift()
+			return {
+				command,
+				args: splitCommand
+			}
+		}
 
-        return {}
-    }
+		return {}
+	}
 }
 
 export default new CommandHandler()
