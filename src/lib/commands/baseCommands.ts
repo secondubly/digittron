b6a939358fb4407cd72ed9af204ad905cbd9d637
secondubly <secondubly@gmail.com>
2024-02-22 17:51:53 -0500

    add command classes

--- src/lib/commands/baseCommands.ts
+++ src/lib/commands/baseCommands.ts
@@ -8,24 +8,28 @@ export const executeBaseCommand = async (command: string, args?: string[]) => {
 		return 'Usage: !editcom (command) (message)'
 	}
 
-	if (command === 'editcom') {
-		const action = args[0]
-		if (!action) {
-			return `Command ${action} does not exist or is not registered.`
-		}
+	switch (command) {
+		case 'editcom': {
+			const action = args[0]
+			if (!action) {
+				return `Command ${action} does not exist or is not registered.`
+			}
 
-		action.replace('!', '').toLowerCase() // remove exclamation point if present
-		const responseMsg = args.slice(1).join(' ')
-		if (!bot.commandCache?.has(action)) {
-			// return error msg
-		}
+			action.replace('!', '').toLowerCase() // remove exclamation point if present
+			const responseMsg = args.slice(1).join(' ')
+			if (!bot.commandCache?.has(action)) {
+				// return error msg
+			}
 
-		const command = bot.commandCache?.get(action) as Command
-		command.response = responseMsg
-		const updatedFields = new Map<string, unknown>([['response', command.response]])
-		await bot.commandCache?.updateCommand(command, updatedFields)
-		return
-	} else if (command === 'songrequest') {
-		// TODO
+			const command = bot.commandCache?.get(action) as Command
+			command.response = responseMsg
+			const updatedFields = new Map<string, unknown>([['response', command.response]])
+			await bot.commandCache?.updateCommand(command, updatedFields)
+			break
+		}
+		default:
+			break
 	}
+
+	return
 }
