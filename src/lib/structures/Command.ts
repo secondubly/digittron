edb531d999fa784cceecb4473cd8197d9abce399
secondubly <secondubly@gmail.com>
2024-02-25 13:58:16 -0500

    add Command class

--- src/lib/structures/Command.ts
+++ src/lib/structures/Command.ts
@@ -1,14 +1,226 @@
-export abstract class Command {
+import { PermissionLevel } from '@prisma/client'
+import { ChatMessage } from './ChatMessage'
+import { DigittronClient } from '../../client'
+
+export interface CommandOptions {
+	/**
+	 * Command name (default alias)
+	 */
+	name: string
+
+	/**
+	 * Userlevel access (viewer (everyone), regular, follower, vip, subscriber, moderator, editor, broadcaster)
+	 */
+	userlevel: keyof typeof PermissionLevel
+
+	/**
+	 * Command description (required for output to !help <command>)
+	 */
+	description?: string
+
+	/**
+	 * Command examples (requited for output to !help <command>)
+	 */
+	examples?: string[]
+
+	/**
+	 * Command arguments
+	 */
+	args?: CommandArgument[]
+
+	/**
+	 * More aliases
+	 */
+	aliases?: string[]
+
+	/**
+	 * Hide command help output to `!commands`
+	 */
+	hideFromHelp?: boolean
+
+	/**
+	 * The command is available only on the bot channel
+	 */
+	botChannelOnly?: boolean
+
+	/**
+	 * TextCommand
+	 */
+	message?: string
+	sendType?: keyof typeof MessageType
+}
+
+export interface CommandArgument {
+	/**
+	 * Alias name
+	 */
+	name: string
+
+	/**
+	 * Value typesafe
+	 */
+	type: StringConstructor | NumberConstructor | BooleanConstructor
+
+	/**
+	 * Default value
+	 */
+	defaultValue?: string | number | boolean
+
+	/**
+	 * Prepare value
+	 */
+	prepare?: (value: unknown, msg?: ChatMessage) => string | number | boolean | void
+}
+
+export enum MessageType {
+	reply = 'reply',
+	actionReply = 'actionReply',
+	say = 'say',
+	actionSay = 'actionSay'
+}
+
+export type NamedParameters = Record<string, string | number | boolean>
+
+export type CommandProvider = Record<string, CommandOptions>
+
+export class BaseCommand {
 	constructor(
-		public name: string,
-		public description: string,
-		private cooldown: number,
-		public enabled: boolean,
-		public permissionLevel: string
+		public client: DigittronClient,
+		public options: CommandOptions
 	) {}
 
-	getCooldown() {
-		return this.cooldown
+	async onPubSub(event: PubSubRedemptionMessage, args?: string[]): Promise<void> {}
+
+	/**
+	 * Method called when execCommand()
+	 *
+	 * @param msg
+	 * @param chatter
+	 */
+	async execute(msg: ChatMessage): Promise<any> {}
+
+	/**
+	 * Method called when command is executed
+	 *
+	 * @param msg
+	 * @param parameters
+	 */
+	async run(msg: ChatMessage, parameters: unknown): Promise<any> {}
+
+	/**
+	 * Prepare the command to be executed
+	 *
+	 * @param msg
+	 * @param parameters
+	 */
+	async prepareRun(msg: ChatMessage, parameters: string[]): Promise<any> {
+		const namedParameters: NamedParameters = {}
+
+		if (this.options.args && this.options.args.length > 0) {
+			for (let i = 0; i < this.options.args.length; i++) {
+				const args = this.options.args[i]
+
+				if (parameters[i]) {
+					if (args.type) {
+						namedParameters[args.name] = args.type(parameters[i])
+					}
+
+					if (args.prepare) {
+						const preparedValue = args.prepare(namedParameters[args.name] || parameters[i])
+
+						if (preparedValue) {
+							namedParameters[args.name] = preparedValue
+						}
+					}
+				} else {
+					if (args.defaultValue) {
+						namedParameters[args.name] = args.defaultValue
+					} else {
+						namedParameters[args.name] = null
+					}
+				}
+			}
+		}
+
+		await this.run(msg, namedParameters)
 	}
-	abstract exec(): unknown
+
+	/**
+	 * Pre validation before to know if command can be executed
+	 *
+	 * @param msg
+	 */
+	preValidate(msg: ChatMessage): string | boolean {
+		// TODO: withWhisper command option
+		if (msg.messageType === 'whisper') {
+			return 'This command can be executed only in the bot channel'
+		}
+
+		if (this.options.botChannelOnly) {
+			if (msg.channel.name !== this.client.getUsername()) {
+				return 'This command can be executed only in the bot channel'
+			}
+		}
+
+		if (this.options.userlevel === PermissionLevel.VIEWER) {
+			return true
+		}
+
+		let validationPassed = false
+
+		if (msg.author.isBroadcaster) {
+			validationPassed = true
+		}
+
+		if (msg.author.isModerator) {
+			validationPassed = true
+		}
+
+		if (this.options.userlevel === PermissionLevel.REGULAR) {
+			if (![...this.client.config.botOwners, this.client.getUsername()].includes(msg.author.username)) {
+				return 'This command can be executed only from bot owners'
+			}
+		}
+
+		if (this.options.userlevel === PermissionLevel.SUBSCRIBER) {
+			if (!validationPassed && !msg.author.isSubscriber) {
+				return 'This command can be executed only from the subscribers'
+			}
+		}
+
+		if (this.options.userlevel === PermissionLevel.VIP) {
+			if (!validationPassed && !msg.author.isVip) {
+				return 'This command can be executed only from the vips'
+			}
+		}
+
+		if (this.options.userlevel === PermissionLevel.MODERATOR) {
+			if (!validationPassed) {
+				return 'This command can be executed only from the broadcaster'
+			}
+		}
+
+		if (this.options.userlevel === PermissionLevel.BROADCASTER) {
+			if (!msg.author.isBroadcaster) {
+				return 'This command can be executed only from a mod or the broadcaster'
+			}
+		}
+
+		return true
+	}
 }
+
+// export abstract class Command {
+// 	constructor(
+// 		public name: string,
+// 		public description: string,
+// 		private cooldown: number,
+// 		public enabled: boolean,
+// 		public permissionLevel: string
+// 	) {}
+
+// 	getCooldown() {
+// 		return this.cooldown
+// 	}
+// 	abstract exec(): unknown
+// }
