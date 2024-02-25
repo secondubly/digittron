22f919d33789ce38d6cad6375cccdeef3ba6c387
secondubly <secondubly@gmail.com>
2024-02-25 16:24:25 -0500

    client adjusrtments

--- src/client.ts
+++ src/client.ts
@@ -1,13 +1,15 @@
 import { createContext } from '@secondubly/digittron-db'
 const { prisma } = await createContext()
 import { AccessToken, RefreshingAuthProvider } from '@twurple/auth'
-import { Client } from '@twurple/auth-tmi'
+import { ChatUserstate, Client } from '@twurple/auth-tmi'
 import { EventSubClient } from './lib/client/EventSubClient.js'
 import { CommandCache } from './lib/structures/CommandCache.js'
 import { redisClient, getUserData } from './lib/utils.js'
 import { ApiClient } from '@twurple/api'
 import { EventEmitter } from 'events'
 import { HelixUserData } from '@twurple/api/lib/interfaces/endpoints/user.external.js'
+import { Logger } from './lib/client/Logger.js'
+import { CommandHandler } from './lib/commands/commandHandler.js'
 
 type DigittronConfig = {
 	prefix: string
@@ -19,18 +21,22 @@ type DigittronConfig = {
 }
 
 export class DigittronClient extends EventEmitter {
-	private id: string
+	private id: string = ''
 	public tmi: Client
 	public api: ApiClient
 	public auth: RefreshingAuthProvider
 	public eventSub: EventSubClient
+	public logger: typeof Logger
 	public commands: CommandCache
 	private config: DigittronConfig
+	private handler: typeof CommandHandler
 	constructor(config: DigittronConfig) {
 		super()
 
 		this.commands = new CommandCache([])
 		this.config = config
+		this.logger = Logger
+		this.handler = CommandHandler
 
 		this.auth = new RefreshingAuthProvider({
 			clientId: this.config.CLIENT_ID,
@@ -43,13 +49,14 @@ export class DigittronClient extends EventEmitter {
 		})
 
 		this.auth.onRefreshFailure((userId) => {
-			console.warn(`User ${userId} needs to reauthenticate!`)
+			this.logger.warn(`User ${userId} needs to reauthenticate!`)
 		})
 
 		this.connect()
 	}
 
 	async updateToken(id: string, token: AccessToken) {
+		this.logger.info(`Token refreshed for ${id}`)
 		const stringToken = JSON.stringify(token)
 		if (this.id === '') {
 			const user = (await getUserData(token)) as HelixUserData
@@ -65,7 +72,7 @@ export class DigittronClient extends EventEmitter {
 	async connect(): Promise<void> {
 		const botAccessToken = this.config.botAccessToken
 		const broadcasterToken: AccessToken = JSON.parse((await redisClient.get('twitch_broadcaster_token')) as string)
-		this.id = (await redisClient.get('twitch_bot_id')) ?? ''
+		this.id = (await redisClient.get('twitch_bot_id')) ?? this.id
 
 		// force token refresh
 		botAccessToken.expiresIn = 0
@@ -100,6 +107,13 @@ export class DigittronClient extends EventEmitter {
 
 		this.eventSub = new EventSubClient(this)
 		await this.eventSub.connect()
+
+		this.tmi.on('message', this.onMessage.bind(this))
+		this.tmi.on('connected', (address: string, port: number) => {
+			this.logger.info(`Connected to ${address}:${port}`)
+		})
+
+		await this.tmi.connect()
 	}
 
 	private async loadCommands(): Promise<void> {
@@ -127,7 +141,7 @@ export class DigittronClient extends EventEmitter {
 
 			this.commands = new CommandCache(parsedCommands)
 		} catch (err) {
-			console.log(err)
+			console.error(err)
 		}
 	}
 
@@ -135,7 +149,27 @@ export class DigittronClient extends EventEmitter {
 		return this.config.channels
 	}
 
+	getUsername() {
+		return this.tmi.getUsername()
+	}
+
+	getBotOwners() {
+		return []
+	}
+
 	async say(channel: string, message: string): Promise<[string]> {
 		return await this.tmi.say(channel, message)
+	}
+
+	private async onMessage(channel: string, tags: ChatUserstate, message: string, self: boolean): Promise<void> {
+		if (self) {
+			return
+		}
+
+		// bot.updateChatActivity(channel)
+
+		if (message.trim().startsWith('!')) {
+			this.handler.run(channel, message, tags.username)
+		}
 	}
 }
