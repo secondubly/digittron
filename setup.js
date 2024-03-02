3244750e20bb01dcb159ed6c32391498499a1ed5
secondubly <secondubly@gmail.com>
2024-03-01 20:59:37 -0500

    docker updates

--- setup.js
+++ setup.js
@@ -26,7 +26,7 @@ let questions = [
     {
         type: 'text',
         name: 'client_id',
-        message: 'Please enter your Twitch Client ID (see https://dev.twitch.tv/console): ',
+        message: 'Enter your Twitch Client ID (see https://dev.twitch.tv/console): ',
         validate: (client_id) => {
             if(client_id.length === 30 || client_id.length === 31) {
                 return true
@@ -37,7 +37,7 @@ let questions = [
     {
         type: 'password',
         name: 'client_secret',
-        message: 'Please enter your Twitch Client Secret (see https://dev.twitch.tv/console): ',
+        message: 'Enter your Twitch Client Secret (see https://dev.twitch.tv/console): ',
         validate: (client_secret) => {
             if(client_secret.length === 30 || client_secret.length === 31) {
                 return true
@@ -48,7 +48,7 @@ let questions = [
     {
         type: 'list',
         name: 'channels',
-        message: 'Please enter the usernames of the twitch channel the bot should join separated by commas',
+        message: 'Enter the usernames of the twitch channel the bot should join separated by commas',
         initial: '',
         separator: ','
     },
@@ -58,7 +58,33 @@ let questions = [
         message: 'What is the bot account\'s username?'
     },
     {
-        
+        type: 'text',
+        name: 'pg_user',
+        message: 'What is your database username? (optional, default: postgres)',
+        initial: 'postgres'
+    },
+    {
+        type: 'text',
+        name: 'db_name',
+        message: 'What is your database name? (optional, default: digittron)',
+        initial: 'digittron'
+    },
+    {
+        type: 'password',
+        name: 'pg_pass',
+        message: 'What is your database password? (optional, default: postgres)',
+        initial: 'postgres'
+    },
+    {
+        type: 'confirm',
+        message: 'Should I use a special port to connect to the database?',
+        initial: false
+    },
+    {
+        type: prev => prev === true ? 'number' : null,
+        name: 'pg_port',
+        message: 'What port should I use?',
+        default: 5432
     }
 ];
 
@@ -79,12 +105,7 @@ async function readWriteAsync(id) {
 
 await (async() => {
     const response = await prompt(questions)
-    const {
-        client_id: clientID, 
-        client_secret: clientSecret,
-        channels: channels
-    } = response
-    console.log('Channels: ', channels)
+    console.log(response)
     // fs.copyFile('./src/.env.example', './src/.env', (err) => {
     //     if (err) {
     //         throw err
