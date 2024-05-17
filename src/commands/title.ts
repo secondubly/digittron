702477449a79c1492e98acecf7e5ebf1548189d5
secondubly <secondubly@gmail.com>
2024-05-17 08:33:00 -0400

    rewrite title command

--- src/commands/title.ts
+++ src/commands/title.ts
@@ -1,49 +1,64 @@
-// TODO: rewrite
-// import { Command } from '../types/command'
-// import { PermissionLevel } from '@prisma/client'
-// import { isNullOrEmpty } from 'lib/utils'
+import { Command } from 'lib/structures/Command'
+import { CommandType, PermissionLevel } from '@prisma/client'
+import { DigittronClient } from 'client'
+import { UserData } from 'types/UserData'
+import { isNullOrEmpty } from 'lib/utils'
 
-// export const title: Command = {
-// 	name: 'test',
-// 	aliases: [],
-// 	permission: PermissionLevel.BROADCASTER,
-// 	callback: async (client, author, channel, ...args: unknown[]): Promise<void> => {
-// 		if (args.length > 0) {
-// 			// TODO: check for mods and broadcaster
-// 			if (author !== 'secondubly') {
-// 				return
-// 			} else {
-// 				const title = args[0] as string
-// 				if (isNullOrEmpty(title)) {
-// 					client.say(channel, 'Invalid command, please try again!')
-// 					return
-// 				}
+class TitleCommand extends Command {
+	id?: string
+	type: CommandType
+	name: string
+	aliases: string[]
+	description?: string
+	permission: PermissionLevel
+	enabled?: boolean
+	hidden?: boolean
 
-// 				const channelData = await client.api.users.getUserByName(channel)
-// 				if (!channelData) {
-// 					throw Error(`Could not get user data for ${channel}`)
-// 				}
+	constructor() {
+		super()
+		this.type = CommandType.DEFAULT
+		this.name = 'title'
+		this.aliases = []
+		this.permission = PermissionLevel.VIEWER
+	}
 
-// 				try {
-// 					await client.api.channels.updateChannelInfo(channelData.id, { title })
-// 					client.say(channel, `@${author} Successfully updated stream title to “${title}”`)
-// 				} catch (e) {
-// 					client.say(channel, 'Something went wrong, sorry!')
-// 				}
-// 			}
-// 		} else {
-// 			const userData = await client.api.users.getUserByName(channel)
-// 			if (!userData) {
-// 				throw Error(`Could not get user data for ${channel}`)
-// 			}
+	async callback(client: DigittronClient, user: UserData, channel: string, ...args: unknown[]): Promise<void> {
+		if (args.length === 0) {
+			const userData = await client.api.users.getUserByName(channel)
+			if (!userData) {
+				throw Error(`Could not get user data for ${channel}`)
+			}
 
-// 			const channelData = await client.api.channels.getChannelInfoById(userData)
-// 			if (!channelData) {
-// 				throw Error(`Could not get stream title for ${channel}`)
-// 			}
+			const channelData = await client.api.channels.getChannelInfoById(userData)
+			if (!channelData) {
+				throw Error(`Could not get stream title for ${channel}`)
+			}
 
-// 			client.say(channel, `@${author} stream title: “${title}”`)
-// 		}
-// 		client.say(channel, 'this is a test of the emergency bot broadcasting system!')
-// 	}
-// }
+			client.say(channel, `@${user.name} stream title: “${channelData.title}”`)
+			return
+		}
+
+		// TODO: check for mods and broadcaster
+		if (user.name !== 'secondubly') {
+			return
+		} else {
+			const title = args[0] as string
+			if (isNullOrEmpty(title)) {
+				client.say(channel, 'Invalid command, please try again!')
+				return
+			}
+
+			const channelData = await client.api.users.getUserByName(channel)
+			if (!channelData) {
+				throw Error(`Could not get user data for ${channel}`)
+			}
+
+			try {
+				await client.api.channels.updateChannelInfo(channelData.id, { title })
+				client.say(channel, `@${user.name} Successfully updated stream title to “${title}”`)
+			} catch (e) {
+				client.say(channel, 'Something went wrong, sorry!')
+			}
+		}
+	}
+}
