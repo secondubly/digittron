230d3cf3779608cf58bc175ec59ad44558e2b0b8
secondubly <secondubly@gmail.com>
2025-06-08 09:09:52 -0700

    feat: initial bot setup

---
+++ src/bot.ts
@@ -0,0 +1,9 @@
+import { RefreshingAuthProvider } from '@twurple/auth'
+
+const CLIENT_SECRET = ''
+const refreshToken = ''
+
+const authProvider = new RefreshingAuthProvider({
+    clientId,
+    clientSecret: CLIENT_SECRET,
+})
