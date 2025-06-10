ef0b4d63ec2d00a169d36085483eeb30746fba3a
secondubly <secondubly@gmail.com>
2025-06-09 22:47:46 -0700

    feat!:add additional .env file variables

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,34 @@
 import * as dotenv from 'dotenv'
 import { resolve } from 'path'
-import { Bot } from './bot'
+import { Bot } from './bot.js'
 
 dotenv.config({ path: resolve(__dirname, '../.env') })
 
 const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
+const BOT_ID = process.env.BOT_ID
+const TWITCH_ID = process.env.TWITCH_ID
 
+const config = {
+    CLIENT_ID,
+    CLIENT_SECRET,
+    BOT_ID,
+    TWITCH_ID,
+}
+
+const hasUndefined = (obj: object) => {
+    return Object.values(obj).includes(undefined)
+}
 const main = async () => {
-    if (!CLIENT_ID || !CLIENT_SECRET) {
+    if (hasUndefined(config)) {
         // TODO: throw error
-        console.log('client id or client secret not available')
+        console.log(
+            'undefined fields found in config file, please check your settings',
+        )
         process.exit(1)
     }
 
-    Bot.init(CLIENT_ID, CLIENT_SECRET)
+    Bot.init(CLIENT_ID!, CLIENT_SECRET!)
 }
 
 main()
