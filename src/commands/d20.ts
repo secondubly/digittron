40c5fb42c77d71ef6d804f0a9eef30c67005d637
secondubly <secondubly@gmail.com>
2025-10-18 21:57:36 -0700

    chore:fix formatting

--- src/commands/d20.ts
+++ src/commands/d20.ts
@@ -1,4 +1,4 @@
-import { Command } from "../types.js"
+import { Command } from '../types.js'
 
 const anniversary: Command = {
     name: 'd20',
@@ -6,8 +6,12 @@ const anniversary: Command = {
     enabled: true,
     async execute(client, channel, msg) {
         const { displayName } = msg.userInfo
-        const roll = Math.floor((Math.random() * 20) + 1)
-        client.say(channel, `${displayName} rolled a ${roll}${roll === 20 ? '! 🎉' : '.'}`)
-    }
+        const roll = Math.floor(Math.random() * 20 + 1)
+        client.say(
+            channel,
+            `${displayName} rolled a ${roll}${roll === 20 ? '! 🎉' : '.'}`,
+        )
+        // TODO: if user rolled a 1, timeout for 1 minute
+    },
 }
 export default anniversary
