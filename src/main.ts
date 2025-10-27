db86d28503a74bc3a10af353ed58f60187e3e11a
secondubly <secondubly@gmail.com>
2025-10-27 12:08:05 -0700

    adjust main for development mode

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+import { fileURLToPath } from 'url'
 import { Bot } from './bot.js'
 import logger from './logger.js'
 import dotenv from 'dotenv'
+import path from 'path'
 
-// TODO: only do this in dev enviroments
-dotenv.config()
+if (process.env.NODE_ENV === 'production') {
+    dotenv.config()
+} else {
+    logger.info('running in development mode')
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+    dotenv.config({ path: __dirname + '/./../.env.development.local' })
+}
 
 const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
@@ -17,20 +25,26 @@ const config = {
     TWITCH_ID,
 }
 
-const hasUndefined = (obj: object) => {
-    return Object.values(obj).includes(undefined)
-}
-const main = async () => {
-    if (hasUndefined(config)) {
-        logger.error(
-            'undefined fields found in config file, please check your settings',
-        )
+const startup = () => {
+    try {
+        const hasUndefined = (obj: object) => {
+            return Object.values(obj).includes(undefined)
+        }
+
+        if (hasUndefined(config)) {
+            logger.error(
+                'undefined fields found in config file, please check your settings',
+            )
+            process.exit(1)
+        }
+
+        if (CLIENT_ID && CLIENT_SECRET) {
+            Bot.init(CLIENT_ID, CLIENT_SECRET)
+        }
+    } catch (e: unknown) {
+        logger.error(`Startup failed: ${(e as Error).message}`)
         process.exit(1)
     }
-
-    if (CLIENT_ID && CLIENT_SECRET) {
-        Bot.init(CLIENT_ID, CLIENT_SECRET)
-    }
 }
 
-main()
+startup()
