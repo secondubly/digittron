48e6207cdfc09135bccd4eb74004f54182b646d0
secondubly <secondubly@gmail.com>
2025-10-27 12:40:28 -0700

    add subcommand for game command

--- src/commands/game.ts
+++ src/commands/game.ts
@@ -1,31 +1,62 @@
-import { Command } from "../types.js"
-import logger from "../logger.js"
-
+import { Command } from '../types.js'
+import logger from '../logger.js'
 
 const game: Command = {
     name: 'game',
     aliases: [],
     enabled: true,
-    async execute(client, channel, msg, _args, apiClient) {
+    async execute(client, channel, msg, args, apiClient) {
         if (!apiClient) {
-            logger.error(`api client not found, cannot execute ${this.name} command`)
+            logger.error(
+                `api client not found, cannot execute ${this.name} command`,
+            )
             return
         }
         const { channelId } = msg
         if (!channelId) {
-            logger.warn('Channel ID not found, is this possibly a private message?')
-            return
-        }
-
-        const channelInfo = await apiClient.channels.getChannelInfoById(channelId)
-        if (!channelInfo) {
-            // log an error
+            logger.warn(
+                'Channel ID not found, is this possibly a private message?',
+            )
             return
         }
 
         const { displayName } = msg.userInfo
-        client.say(channel, `@${displayName}, current game: ${channelInfo.gameName}`)
-    }
+        if (args.length === 0) {
+            const channelInfo =
+                await apiClient.channels.getChannelInfoById(channelId)
+            if (!channelInfo) {
+                // log an error
+                return
+            }
+
+            client.say(
+                channel,
+                `@${displayName}, current game: ${channelInfo.gameName}`,
+            )
+        } else {
+            const gameTitle = args.join()
+            const gameData = await apiClient.games.getGamesByNames([gameTitle])
+            if (!gameData.length) {
+                logger.warn(
+                    `Could not find any games with the title ${gameTitle}`,
+                )
+                client.say(
+                    channel,
+                    `@${displayName} could not find any games with that title. Please check your input and try again.`,
+                )
+                return
+            }
+
+            apiClient.channels.updateChannelInfo(channelId, {
+                gameId: gameData[0].id,
+            })
+
+            client.say(
+                channel,
+                `@${displayName} updated game title to: ${gameData[0].name}.`,
+            )
+        }
+    },
 }
 
 export default game
