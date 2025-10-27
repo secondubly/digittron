cbc8bccb342b5de518700c6a666ee96d23974301
secondubly <secondubly@gmail.com>
2025-10-27 13:49:19 -0700

    add subcommand to title command

--- src/commands/title.ts
+++ src/commands/title.ts
@@ -1,31 +1,53 @@
-import { Command } from "../types.js"
-import logger from "../logger.js"
-
+import { Command } from '../types.js'
+import logger from '../logger.js'
 
 const title: Command = {
     name: 'title',
     aliases: [],
     enabled: true,
-    async execute(client, channel, msg, _args, apiClient) {
+    async execute(client, channel, msg, args, apiClient) {
         if (!apiClient) {
-            logger.error(`api client not found, cannot execute  ${this.name} command`)
+            logger.error(
+                `api client not found, cannot execute  ${this.name} command`,
+            )
             return
         }
 
         const { channelId } = msg
         if (!channelId) {
-            logger.warn('Channel ID not found, is this possibly a private message?')
+            logger.warn(
+                'Channel ID not found, is this possibly a private message?',
+            )
             return
         }
 
-        const channelInfo = await apiClient.channels.getChannelInfoById(channelId)
+        const channelInfo =
+            await apiClient.channels.getChannelInfoById(channelId)
         if (!channelInfo) {
             return
         }
 
         const { displayName } = msg.userInfo
-        client.say(channel, `@${displayName}, title: ${channelInfo.title}`)
-    }
+        if (!args.length) {
+            client.say(channel, `@${displayName}, title: ${channelInfo.title}`)
+        } else {
+            const { isMod, isBroadcaster } = msg.userInfo
+            if (!isMod && !isBroadcaster) {
+                return
+            }
+
+            const streamTitle = args.join()
+
+            apiClient.channels.updateChannelInfo(channelId, {
+                title: streamTitle,
+            })
+
+            client.say(
+                channel,
+                `@${displayName} updated game title to: ${streamTitle}.`,
+            )
+        }
+    },
 }
 
 export default title
