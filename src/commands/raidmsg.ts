6a5c0106a4db0cacf0462e387358f9f5080068a6
secondubly <secondubly@gmail.com>
2025-10-30 19:34:08 -0700

    remove mod check for raid message

--- src/commands/raidmsg.ts
+++ src/commands/raidmsg.ts
@@ -4,11 +4,7 @@ const raidmsg: Command = {
     name: 'raidmsg',
     aliases: [],
     enabled: true,
-    async execute(client, channel, msg, _args, _apiClient) {
-        const { isMod, isBroadcaster } = msg.userInfo
-        if (!(isMod || isBroadcaster)) {
-            return
-        }
+    async execute(client, channel, _msg, _args, _apiClient) {
         client.say(
             channel,
             'raid message: second15RAID second15RAID second15RAID 01010010 01000001 01001001 01000100 00100001 00100001 00100001 second15RAID second15RAID second15RAID',
