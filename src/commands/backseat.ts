9137744130f9d829b9ca12b603d86f9ba20e649c
secondubly <secondubly@gmail.com>
2025-11-08 19:43:51 -0800

    fix backseat command

--- src/commands/backseat.ts
+++ src/commands/backseat.ts
@@ -1,13 +1,15 @@
-import { Command } from "../types.js"
-
+import { Command } from '../types.js'
 
 const backseat: Command = {
-    name: 'test',
+    name: 'backseat',
     aliases: [],
     enabled: true,
     async execute(client, channel, _args, _apiClient) {
-        client.say(channel, `please do not backseat the streamer! when @${channel} needs help, they will ask for it! thank you!`)
-    }
+        client.say(
+            channel,
+            `please do not backseat the streamer! when @${channel} needs help, they will ask for it! thank you!`,
+        )
+    },
 }
 
 export default backseat
