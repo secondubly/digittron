a4fa8ac1668fde33d7182bcb82305f1997808645
secondubly <secondubly@gmail.com>
2025-12-25 22:30:09 -0800

    add token api endpoint, formatting fixes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,147 +1,198 @@
-import fastify, {type FastifyBaseLogger, type FastifyInstance, type FastifyReply, type FastifyRequest } from "fastify"
-import cors from '@fastify/cors'
-import config from '../mikro-orm.config.js'
-import { MikroORM } from '@mikro-orm/sqlite'
-import fastifySSE from "@fastify/sse"
-import { Token } from '../lib/db/models/token.entity.js'
-import type { AccessToken } from '@twurple/auth'
-import { log } from "@lib/utils/logger.js"
+import fastify, {
+  type FastifyBaseLogger,
+  type FastifyInstance,
+  type FastifyReply,
+  type FastifyRequest,
+} from "fastify";
+import cors from "@fastify/cors";
+import config from "../mikro-orm.config.js";
+import { MikroORM } from "@mikro-orm/sqlite";
+import fastifySSE from "@fastify/sse";
+import { Token } from "../lib/db/models/token.entity.js";
+import type { AccessToken } from "@twurple/auth";
+import { log } from "@lib/utils/logger.js";
 
 interface Client {
-    id: number
-    response: FastifyReply
+  id: number;
+  response: FastifyReply;
 }
 
-type AudioRequestParams = {
-  twitchId: string
+interface TokenQueryString {
+  scopes: string;
 }
+
+type RequestParams = {
+  id: string;
+};
 
 // setup database connection
-const orm = await MikroORM.init(config)
-const em = orm.em.fork()
-let clients: Client[] = []
+const orm = await MikroORM.init(config);
+const em = orm.em.fork();
+let clients: Client[] = [];
 
 const twitchAudioMap: Map<string, string> = new Map([
-    ['537326154', '537326154.wav'],
-])
+  ["537326154", "537326154.wav"],
+]);
 
 const sendAudioUpdates = (data: string) => {
-    clients.forEach((client) => {
-        client.response.sse.send({
-            event: 'play',
-            data,
-        })
-    })
-}
+  clients.forEach((client) => {
+    client.response.sse.send({
+      event: "play",
+      data,
+    });
+  });
+};
 
 export const routes = {
   async getToken(id: string, scopes: string) {
-    const scopesArray = scopes ? scopes.split(',') : undefined
-    const tokensTable = em.getRepository(Token)
+    const scopesArray = scopes ? scopes.split(",") : undefined;
+    const tokensTable = em.getRepository(Token);
+
     const token = await tokensTable.findOne({
-        id: parseInt(id),
-    })
+      id: parseInt(id),
+    });
 
     if (!token) {
-        return null
+      return null;
     }
 
-    let accessToken = undefined
+    let accessToken = undefined;
     if (scopesArray) {
-        accessToken = {
-            accessToken: token.accessToken,
-            refreshToken: token.refreshToken,
-            scope: scopesArray,
-            expiresIn: 0,
-            obtainmentTimestamp: 0,
-        } as AccessToken
+      accessToken = {
+        accessToken: token.accessToken,
+        refreshToken: token.refreshToken,
+        scope: scopesArray,
+        expiresIn: 0,
+        obtainmentTimestamp: 0,
+      } as AccessToken;
     } else {
-        accessToken = {
-            accessToken: token.accessToken,
-            refreshToken: token.refreshToken,
-            expiresIn: 0,
-            obtainmentTimestamp: 0,
-        } as AccessToken
+      accessToken = {
+        accessToken: token.accessToken,
+        refreshToken: token.refreshToken,
+        expiresIn: 0,
+        obtainmentTimestamp: 0,
+      } as AccessToken;
     }
 
-    return accessToken
+    return accessToken;
   },
   getAudio(id: string) {
-    return twitchAudioMap.get(id)
-  }
-}
+    return twitchAudioMap.get(id);
+  },
+};
 
 export const init = async (port: number) => {
-  console.log(`Iniitializing API on port ${port}`)
-  let server: FastifyInstance
-  if (process.env.NODE_ENV === 'development') {
+  console.log(`Iniitializing API on port ${port}`);
+
+  let server: FastifyInstance;
+  if (process.env.NODE_ENV === "development") {
     server = fastify({
-        loggerInstance: log.api as FastifyBaseLogger
-    })
+      loggerInstance: log.api as FastifyBaseLogger,
+    });
   } else {
-    server = fastify()
+    server = fastify();
   }
 
   await server.register(cors, {
-      origin: ['http://localhost:5000', 'http://localhost:5001', 'http://localhost:5173'],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  })
+    origin: [
+      "http://localhost:5000",
+      "http://localhost:5001",
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  });
 
-  await server.register(fastifySSE)
+  await server.register(fastifySSE);
 
   // GET /ping - test endpoint
-  server.get('/ping', async (_request, _reply) => {
-    return 'pong\n'
-  })
+  server.get("/ping", async (_request, _reply) => {
+    return "pong\n";
+  });
+
+  server.get<{
+    Params: RequestParams;
+    Querystring: TokenQueryString;
+  }>("/api/token/:id", async (request, reply) => {
+    const { id } = request.params;
+    const { scopes } = request.query;
+
+    const token = await routes.getToken(id, scopes);
+
+    if (!token) {
+      reply.code(404).send({ error: "Token not found" });
+    } else {
+      let accessToken;
+      if (scopes) {
+        accessToken = {
+          accessToken: token.accessToken,
+          refreshToken: token.refreshToken,
+          scope: scopes.split(","),
+          expiresIn: 0,
+          obtainmentTimestamp: 0,
+        } as AccessToken;
+      } else {
+        accessToken = {
+          accessToken: token.accessToken,
+          refreshToken: token.refreshToken,
+          expiresIn: 0,
+          obtainmentTimestamp: 0,
+        } as AccessToken;
+      }
+
+      reply.code(200).send({ token: accessToken });
+    }
+
+    return;
+  });
 
   // GET /api/audio/:twitchId
-  server.get('/api/audio/:twitchId', async (request: FastifyRequest<{ Params: AudioRequestParams }>, reply) => {
-    const { twitchId } = request.params
-    // TODO: grab filename
-    log.api.info(
+  server.get(
+    "/api/audio/:twitchId",
+    async (request: FastifyRequest<{ Params: RequestParams }>, reply) => {
+      const { id: twitchId } = request.params;
+      // TODO: grab filename
+      log.api.info(
         `Server received playback request for twitch id: ${twitchId}`,
-    )
-    const audioFilename = routes.getAudio(twitchId)
-    if (!audioFilename) {
+      );
+      const audioFilename = routes.getAudio(twitchId);
+      if (!audioFilename) {
         reply
-            .code(404)
-            .send({ error: `Audio file not found for id: ${twitchId}` })
-    } else {
-        sendAudioUpdates(twitchId)
-        reply.code(200).send()
-    }
-  })
-
-  server.get('/events', { sse: true }, async (_request, reply) => {
-      reply.sse.keepAlive()
-      await reply.sse.send({ data: 'Connected' })
-
-      const clientId = Date.now()
-      const newClient: Client = {
-          id: clientId,
-          response: reply,
+          .code(404)
+          .send({ error: `Audio file not found for id: ${twitchId}` });
+      } else {
+        sendAudioUpdates(twitchId);
+        reply.code(200).send();
       }
-      clients.push(newClient)
+    },
+  );
 
-      reply.sse.onClose(() => {
-          log.api.info(`SSE Client closed: ${clientId}`)
-          clients = clients.filter((client) => clientId !== client.id)
-      })
-  })
+  server.get("/events", { sse: true }, async (_request, reply) => {
+    reply.sse.keepAlive();
+    await reply.sse.send({ data: "Connected" });
 
+    const clientId = Date.now();
+    const newClient: Client = {
+      id: clientId,
+      response: reply,
+    };
+    clients.push(newClient);
 
-
+    reply.sse.onClose(() => {
+      log.api.info(`SSE Client closed: ${clientId}`);
+      clients = clients.filter((client) => clientId !== client.id);
+    });
+  });
 
   return server.listen({ port }, (err, address) => {
-        if (err) {
-            console.error(err)
-            process.exit(1)
-        }
-        console.log(`API server listening at ${address}`)
-    })
-}
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`API server listening at ${address}`);
+  });
+};
 
 if (import.meta.main) {
-  const port = process.env.API_PORT ?? '4001'
-  init(parseInt(port))
+  const port = process.env.API_PORT ?? "4001";
+  init(parseInt(port));
 }
