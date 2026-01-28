c77e9845e7c19aa69c810c5d773938a224c5ca77
secondubly <secondubly@gmail.com>
2026-01-28 11:55:34 -0800

    update db paths

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,31 +1,32 @@
 import { type Options, SqliteDriver } from '@mikro-orm/sqlite'
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection'
 import { Migrator } from '@mikro-orm/migrations'
+import { SeedManager } from '@mikro-orm/seeder'
 
 const config: Options = {
     driver: SqliteDriver,
-    dbName: './data/sqlite.db',
+    dbName: './db/sqlite.db',
     // folder-based discovery setup, using common filename suffix
-    entities: ['build/**/*.entity.js'],
-    entitiesTs: ['src/**/*.entity.ts'],
+    entities: ['build/lib/db/models/*.entity.js'],
+    entitiesTs: ['src/lib/db/models/*.entity.ts'],
     // we will use the ts-morph reflection, an alternative to the default reflect-metadata provider
     // check the documentation for their differences: https://mikro-orm.io/docs/metadata-providers
     metadataProvider: TsMorphMetadataProvider,
     // enable debug to log sql queries and discovery information
-    debug: true,
-    extensions: [Migrator],
+    debug: process.env.NODE_ENV === 'development' ? true : false,
+    extensions: [Migrator, SeedManager],
     migrations: {
         tableName: 'digittron_migrations',
-        path: './build/data/migrations',
-        pathTs: './data/migrations',
+        path: './build/lib/db/data/migrations',
+        pathTs: './src/lib/db/data/migrations',
         glob: '*.{js,ts}',
     },
     seeder: {
-        path: './build/data/seeders',
-        pathTs: './data/seeders/',
+        path: './build/lib/db/seeders',
+        pathTs: './src/lib/db/seeders',
         defaultSeeder: 'DatabaseSeeder',
-        glob: '*.{js,ts}',
-        emit: 'ts',
+        glob: '!(*.d).{js,ts}',
+        emit: process.env.NODE_ENV === 'development' ? 'ts' : 'js',
     },
 }
 
