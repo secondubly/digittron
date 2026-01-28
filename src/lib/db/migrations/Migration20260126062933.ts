24b32e34fc1f8477dea8cf8e364138f62a0a0001
tai m. <secondubly@gmail.com>
2026-01-28 15:46:13 -0800

    update migration - create table ONLY if it doesn't exist

--- src/lib/db/migrations/Migration20260126062933.ts
+++ src/lib/db/migrations/Migration20260126062933.ts
@@ -3,7 +3,7 @@ import { Migration } from '@mikro-orm/migrations';
 export class Migration20260126062933 extends Migration {
 
   override async up(): Promise<void> {
-    this.addSql(`create table \`token\` (\`id\` integer not null primary key autoincrement, \`access_token\` text not null, \`refresh_token\` text not null);`);
+    this.addSql(`create table if not exists \`token\` (\`id\` integer not null primary key autoincrement, \`access_token\` text not null, \`refresh_token\` text not null);`);
   }
 
 }
