a3d41b9b9db48344023dab3496953dfc8d99f56e
secondubly <secondubly@gmail.com>
2026-02-06 10:30:34 -0800

    move then to newline?

--- initdb.sh
+++ initdb.sh
@@ -1,7 +1,8 @@
 DB_PATH="/usr/src/app/sqlite.db"
 
 # Check if the file does not exist
-if [ ! -f "$DB_PATH" ]; then
+if [ ! -f "$DB_PATH" ]; 
+then
     echo "Database file not found. Creating a new one at $DB_PATH"
     # Create the database file by running a simple, harmless command like 'VACUUM;' or 'PRAGMA user_version = 0;'
     sqlite3 "$DB_PATH" "VACUUM;"
