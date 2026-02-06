b1710b5e12c549ebf23a98cdd9e75f1957fa24ec
secondubly <secondubly@gmail.com>
2026-02-06 09:54:06 -0800

    testing init script fixes

--- initdb.sh
+++ initdb.sh
@@ -1,9 +1,6 @@
 if [ -f .env.development.local ]; then
     echo '.env.development.local file found'
     export $(grep -v '^#' .env.development.local | xargs)
-elif [ -f .env ]; then
-    echo '.env file found'
-    export $(grep -v '^#' .env | xargs)
 fi
 
 DB_PATH="./db/sqlite.db"
