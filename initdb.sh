afbf0e53ce53c50e855ff1e1a7fd82590a1b55c9
secondubly <secondubly@gmail.com>
2026-02-06 11:11:41 -0800

    attempt to create folder before sqlite command

--- initdb.sh
+++ initdb.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env sh
 
-DB_PATH="/usr/src/app/db/sqlite.db"
+
+DB_PATH="/usr/src/app/db"
+
+if [ ! -d "$DB_PATH" ]; then
+  echo "Database folder not found, creating folder..."
+  mkdir -p "$DB_PATH";
+fi
 
 # Check if the file does not exist
 if [ ! -f "$DB_PATH" ]; then
     echo "Database file not found. Creating a new one at $DB_PATH"
-    sqlite3 "$DB_PATH" "VACUUM;"
+
+    sqlite3 "$DB_PATH/sqlite.db" "VACUUM;"
 else
     echo "Database file found at $DB_PATH"
 fi
