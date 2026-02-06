b7ef8bc30f802df2efe6ec82548c1b5857f3c09d
secondubly <secondubly@gmail.com>
2026-02-06 09:59:22 -0800

    remove commented lines

--- initdb.sh
+++ initdb.sh
@@ -1,12 +1,3 @@
-# if [ -f .env.development.local ]; then
-#     echo '.env.development.local file found'
-#     export $(grep -v '^#' .env.development.local | xargs)
-# elif [ -f .env ]; then
-#     echo '.env file found'
-#     export $(grep -v '^#' .env | xargs)
-# fi
-
-
 export $(grep -v '^#' .env | xargs)
 DB_PATH="./db/sqlite.db"
 
