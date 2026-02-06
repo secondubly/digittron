8bf37f1b98299cb84d8313cbe6ad1e019c00f936
secondubly <secondubly@gmail.com>
2026-02-06 10:23:47 -0800

    initdb script fix and change bot container name

--- initdb.sh
+++ initdb.sh
@@ -1,6 +1,10 @@
-DB_FILE="sqlite.db"
+DB_PATH="/usr/src/app/sqlite.db"
 
-DB_DIR=$(dirname "$DB_FILE")
-mkdir -p "/usr/src/app/db"
-
-sqlite3 "$DB_FILE" ";"
+# Check if the file does not exist
+if [ ! -f "$DB_PATH" ]; then
+    echo "Database file not found. Creating a new one at $DB_PATH"
+    # Create the database file by running a simple, harmless command like 'VACUUM;' or 'PRAGMA user_version = 0;'
+    sqlite3 "$DB_PATH" "VACUUM;"
+else
+    echo "Database file found at $DB_PATH"
+fi
