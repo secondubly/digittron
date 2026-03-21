1a90c9ac57dede6eb66e251abe3e88a3c24e107f
secondubly <secondubly@gmail.com>
2026-03-20 23:00:50 -0700

    add (hidden) registration page

---
+++ src/web/react/pages/Register.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RegistrationForm from '../components/ui/RegistrationForm';
+
+const Register: React.FC = () => {
+  return (
+    <>
+    <h2>Register</h2>
+
+    <RegistrationForm />
+    </>
+  );
+};
+
+export default Register;
