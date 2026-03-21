ca07c2794ed4601ae0930c396d6ca0af5332d8e5
secondubly <secondubly@gmail.com>
2026-03-20 23:02:13 -0700

    remove props from login form now that we have AuthContext

--- src/web/react/components/ui/LoginForm.tsx
+++ src/web/react/components/ui/LoginForm.tsx
@@ -1,22 +1,19 @@
-import React from "react";
-import { Stack,  PasswordInput, TextInput, ActionIcon, Modal, Button } from "@mantine/core";
+import React, { useState } from "react";
+import { Stack,  PasswordInput, TextInput, ActionIcon, Modal, Button, LoadingOverlay, Text, Group } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { notifications } from '@mantine/notifications'
 import { IconLogin2, IconLogout2 } from "@tabler/icons-react";
 import { useDisclosure } from "@mantine/hooks";
-import type { Credentials, Token } from '../../types/loginTypes'
-import { useNavigate } from "react-router-dom";
+import type { Credentials } from '../../types/loginTypes'
+import { useNavigate } from "react-router-dom"; 
+import { useAuth } from '../../contexts/AuthContext'
 
-
-
-interface Props {
-    setToken: (token: Token) => void
-    removeToken: () => void
-    isLoggedIn: boolean
-}
-const LoginForm: React.FC<Props> = ({ setToken, removeToken, isLoggedIn }) => {
+const LoginForm = () => {
     const [opened, { open, close }] = useDisclosure(false);
-    const navigate = useNavigate()
+    const [visible, { open: loaderOpen, close: loaderClose }] = useDisclosure(false);
+    const [error, setError] = useState<string | null>(null);
+    const navigate = useNavigate();
+    const { login, logout, isAuthenticated } = useAuth()
     const form = useForm<Credentials>({
     mode: 'uncontrolled',
     initialValues: {
@@ -25,27 +22,22 @@ const LoginForm: React.FC<Props> = ({ setToken, removeToken, isLoggedIn }) => {
     }
     });
 
-    const loginUser = (credentials: Credentials) => {
-        return fetch('http://localhost:4000/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(credentials)
-        }).then(data => data.json())
-    }
-
     const handleSubmit = async (values: Credentials) => {
-        const token = await loginUser(values)
-        if (token) {
+        try {
+            loaderOpen()
+            await login(values)
             close()
+        } catch(e) {
+            if (e instanceof Error) {
+                setError(e.message || 'An error occurred during login.')
+            }
+        } finally {
+            loaderClose()
         }
-        console.log(token)
-        setToken(token)
     }
 
     const handleLogout = () => {
-        removeToken()
+        logout()
         navigate('/home')
         notifications.show({
             message: 'You have successfully logged out.'
@@ -56,6 +48,7 @@ const LoginForm: React.FC<Props> = ({ setToken, removeToken, isLoggedIn }) => {
     return (
         <>
             <Modal opened={opened} onClose={close} title='Login to your Account' centered>
+                <LoadingOverlay h={'100%'} visible={visible} loaderProps={{ children: 'Loading...' }} />
                 <form onSubmit={form.onSubmit(handleSubmit)}>
                     <Stack                     
                     gap='md'
@@ -64,22 +57,34 @@ const LoginForm: React.FC<Props> = ({ setToken, removeToken, isLoggedIn }) => {
                         label="Username"
                         placeholder="username"
                         key={form.key('username')}
-                            {...form.getInputProps('username')} />
+                            {...form.getInputProps('username')} 
+                        required />
 
                         <PasswordInput
                             label="Password"
                             placeholder="password"
                             key={form.key('password')}
-                            {...form.getInputProps('')}
+                            {...form.getInputProps('password')}
+                            required
                         />
+                        
+                        <Group justify="space-between">
+                        {
+                            error && (
+                                <Text size="sm" c="red">{error}</Text>
+                            )
+                        }
 
-                        <Button type="submit" w='fit-content' style={{'alignSelf': 'flex-end'}}>Submit</Button>
+                        <Button type="submit" w='fit-content' 
+                        style={{'alignSelf': 'flex-end'}} 
+                        disabled={visible}>Log In</Button>
+                        </Group>
                     </Stack>
-            </form>
+                </form>
             </Modal>
 
-            <ActionIcon size='xl' radius='sm' onClick={isLoggedIn ? open : handleLogout}>
-            { isLoggedIn ? <IconLogout2 /> : <IconLogin2 /> }
+            <ActionIcon size='xl' radius='sm' onClick={isAuthenticated ? handleLogout : open}>
+            { isAuthenticated ? <IconLogout2 /> : <IconLogin2 /> }
             </ActionIcon>
         </>
     )
