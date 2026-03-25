d61b320931ee641b98d91ca1aac4dd24adcafa3a
secondubly <secondubly@gmail.com>
2026-03-25 16:12:21 -0700

    add public assets folder for fastify-static

---
+++ src/server/plugins/external/static-assets.ts
@@ -0,0 +1,15 @@
+import fastifyStatic, { type FastifyStaticOptions } from '@fastify/static'
+import path from 'node:path'
+import fp from 'fastify-plugin'
+import type { FastifyRegisterOptions } from 'fastify'
+
+const dirPath = path.join(import.meta.dirname, '../../', 'public', 'assets')
+const staticOpts: FastifyRegisterOptions<FastifyStaticOptions> = {
+    root: dirPath,
+    prefix: '/public/',
+    decorateReply: false,
+}
+
+export default fp(async (fastify) => {
+    fastify.register(fastifyStatic, staticOpts)
+})
---
+++ src/server/plugins/external/static-web.ts
@@ -0,0 +1,14 @@
+import fastifyStatic, { type FastifyStaticOptions } from '@fastify/static'
+import path from 'node:path'
+import fp from 'fastify-plugin'
+import type { FastifyRegisterOptions } from 'fastify'
+
+const dirPath = path.join(import.meta.dirname, '../../../../', 'build', 'web')
+const staticOpts: FastifyRegisterOptions<FastifyStaticOptions> = {
+    root: dirPath,
+    prefix: '/',
+}
+
+export default fp(async (fastify) => {
+    fastify.register(fastifyStatic, staticOpts)
+})
