3dd72c86765b4470222520c74a6e5faed01d292f
secondubly <secondubly@gmail.com>
2026-03-27 22:30:23 -0700

    replace placeholder image

--- src/web/react/components/navbar/user-button/UserButton.tsx
+++ src/web/react/components/navbar/user-button/UserButton.tsx
@@ -1,13 +1,14 @@
 import { IconChevronRight } from '@tabler/icons-react';
 import { Avatar, Group, Text, UnstyledButton } from '@mantine/core';
 import classes from './UserButton.module.css';
+import placeholder from '../../../assets/images/h_jjk.jpg'
 
 export function UserButton() {
   return (
     <UnstyledButton className={classes.user} p="md">
       <Group>
         <Avatar
-          src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png"
+          src={placeholder}
           radius="xl"
           alt="Harriette Spoonlicker"
         />
