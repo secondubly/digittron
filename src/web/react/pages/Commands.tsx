5ec6107dcc74d265b7afbc67401ac6a6320a0030
secondubly <secondubly@gmail.com>
2026-03-28 23:35:37 -0700

    forgot to commit commands page update

--- src/web/react/pages/Commands.tsx
+++ src/web/react/pages/Commands.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { CommandsTable } from '../components/ui/CommandsTable';
 
 const Commands: React.FC = () => {
-  return <h2>Commands Page</h2>;
+  return (
+    <>
+      <h2>Commands Page</h2>
+      <CommandsTable />
+    </>
+  )
 };
 
 export default Commands;
