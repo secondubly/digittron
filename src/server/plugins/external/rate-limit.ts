10ab3df76bb08a36b5abaad02457894a321c44bd
secondubly <secondubly@gmail.com>
2026-03-28 20:23:02 -0700

    add rate limiting

---
+++ src/server/plugins/external/rate-limit.ts
@@ -0,0 +1,15 @@
+import fastifyRateLimit from '@fastify/rate-limit'
+import { type FastifyInstance } from 'fastify'
+
+export const autoConfig = (fastify: FastifyInstance) => {
+    return {
+        // any IP can make at most 4 requests per minute
+        max: fastify.config.RATE_LIMIT_MAX,
+        timeWindow: '1 minute',
+    }
+}
+
+/**
+ * @see {@link https://github.com/fastify/fastify-rate-limit}
+ */
+export default fastifyRateLimit
