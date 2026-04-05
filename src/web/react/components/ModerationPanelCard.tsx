31a94e9602ce20a63116c75d867dd59dd9bf616c
secondubly <secondubly@gmail.com>
2026-04-04 20:46:22 -0700

    initial moderation page setup

--- src/web/react/components/ModerationPanelCard.tsx
+++ src/web/react/components/ModerationPanelCard.tsx
@@ -1,5 +1,6 @@
-import { Box, Card, Flex, Stack, Text, Title } from "@mantine/core";
+import { Card, Divider, Flex, Group, Stack, Switch, Text, Title } from "@mantine/core";
 import { IconHome } from "@tabler/icons-react";
+import { useState } from "react";
 
 interface Props {
     icon: typeof IconHome
@@ -8,19 +9,24 @@ interface Props {
     enabled: boolean
 }
 
-export const ModerationPanelCard = ({ icon: IconComponent, title, description, enabled }: Props) => {
+export const ModerationPanelCard = ({ icon: IconComponent, title, description, enabled: on }: Props) => {
+    const [enabled, setEnabled] = useState(on)
     return (
-        <Card radius={'sm'} padding={25}>
+        <Card radius={'sm'} p={0}>
             <Stack>
-                <Flex gap={'sm'} align={'flex-start'}>
-                    <Box>
-                        <IconComponent />
-                    </Box>
+                <Flex gap={'sm'} align={'flex-start'} p={25}>
+                    <IconComponent className="moderation-icon" size={18} />
                     <Stack gap={'xs'}>
                         <Title size={'md'} fw={'bold'}>{title}</Title>
                         <Text>{description}</Text>
                     </Stack>
                 </Flex>
+                <Divider />
+                <Group px={24} py={10} align={'center'}>
+                    <Switch withThumbIndicator checked={enabled} 
+                    label={enabled ? 'Enabled' : 'Disabled'}
+                    onChange={(event) => (setEnabled(event.currentTarget.checked))} />
+                </Group>
             </Stack>
         </Card>
     )
