75dbf7aa2881138d86f8d50ec8de5fea09a3dad2
secondubly <secondubly@gmail.com>
2026-04-07 17:58:02 -0700

    add NotFound handler and formatting fixes

--- src/server/build.ts
+++ src/server/build.ts
@@ -1,15 +1,8 @@
 import path from 'node:path'
 import fastifyAutoload from '@fastify/autoload'
-import type {
-    FastifyError,
-    FastifyInstance,
-    FastifyPluginOptions,
-} from 'fastify'
+import type { FastifyError, FastifyInstance, FastifyPluginOptions } from 'fastify'
 
-export default async function bootstrap(
-    fastify: FastifyInstance,
-    opts: FastifyPluginOptions,
-) {
+export default async function bootstrap(fastify: FastifyInstance, opts: FastifyPluginOptions) {
     delete opts.skipOverride
     /**
      * load external plugins first because the server may need them immediately
@@ -53,4 +46,31 @@ export default async function bootstrap(
 
         return { message }
     })
+
+    // An attacker could search for valid URLs if error handling is not rate limited.
+    fastify.setNotFoundHandler(
+        {
+            preHandler: fastify.rateLimit({
+                max: 3,
+                timeWindow: 500,
+            }),
+        },
+        (request, reply) => {
+            request.log.warn(
+                {
+                    request: {
+                        method: request.method,
+                        url: request.url,
+                        query: request.query,
+                        params: request.params,
+                    },
+                },
+                'Resource not found',
+            )
+
+            reply.code(404)
+
+            return { message: 'Not Found' }
+        },
+    )
 }
