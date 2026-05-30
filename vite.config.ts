1beb513876368a35dfe16ae94312ba1b849f5168
secondubly <secondubly@gmail.com>
2026-05-30 13:44:39 -0400

    add proxy for api endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,11 @@ export default defineConfig({
                 changeOrigin: true,
                 secure: false,
             },
+            '/api': {
+                target: 'http://localhost:4000/',
+                changeOrigin: true,
+                secure: false,
+            },
         },
     },
 })
