b07d39acbe225037f3e5a6067de58484db8bbbca
secondubly <secondubly@gmail.com>
2026-06-02 13:36:27 -0400

    basic song request queue setup

--- src/web/react/components/spotifyqueue/SpotifyQueue.tsx
+++ src/web/react/components/spotifyqueue/SpotifyQueue.tsx
@@ -1,7 +1,23 @@
 import useSWR from 'swr'
+import classes from './SpotifyQueue.module.css'
 import { type QueueResponse, type SimplifiedTrack } from '../../types/spotify'
+import { Box, Flex, Grid, Group, Image, SimpleGrid, Stack, Text, Title } from '@mantine/core'
 
-const spotifyFetcher = async (url: string, token: string, ) => {
+const getToken = async () => {
+  try {
+      const twitchId = '89181064'
+      const res = await fetch(`/api/spotify/token/${twitchId}`)
+
+      const token = await res.text()
+      return token
+  } catch (error) {
+      console.error('Failed to retrieve token', error)
+  }
+}
+
+const spotifyFetcher = async (url: string) => {
+  try {
+    const token = await getToken()
     const res = await fetch(url, {
         headers: {
             Authorization: `Bearer ${token}`,
@@ -9,21 +25,40 @@ const spotifyFetcher = async (url: string, token: string, ) => {
     })
 
     if (!res.ok) {
-        throw new Error('An error occurred while fetching the data.')
+      if (res.status === 401) {
+        const newToken = await getToken()
+
+        const retryResponse = await fetch(url, {
+          headers: {
+              Authorization: `Bearer ${newToken}`,
+          },
+        })
+
+        if (!retryResponse.ok) {
+          throw new Error(retryResponse.statusText)
+        }
+
+        return retryResponse.json()
+      }
+      throw new Error(res.statusText)
     }
 
     return res.json()
+  } catch (err) {
+    throw err
+  }
 }
 
 
-const SpotifyQueue = ({ accessToken }: { accessToken: string | null }) => {
-  // SWR constructor: (url endpoint (aka key), fetch HTTP request to call on the endpoint (aka key), {optional parameters})
-  useSWR(accessToken, )
-  const { data, error, isLoading } = useSWR<QueueResponse>(
-    accessToken ? ['https://api.spotify.com/v1/me/player/queue', accessToken] : null,
-    ([url, token]: string[]) => spotifyFetcher(url, token),
+const formatTime = (ms: number) => {
+  return new Date(ms).toISOString().slice(14, 19)
+}
+
+const SpotifyQueue = () => {
+  // SWR constructor: (url endpoint (aka key), fetch HTTP request to call on the endpoint, {optional parameters})
+  const { data, error, isLoading } = useSWR<QueueResponse>('https://api.spotify.com/v1/me/player/queue', spotifyFetcher,
     {
-      refreshInterval: 5000, // Polls every 5 seconds to stay in sync with Spotify
+      refreshInterval: 5000,
       revalidateOnFocus: false,
     }
   );
@@ -33,31 +68,45 @@ const SpotifyQueue = ({ accessToken }: { accessToken: string | null }) => {
   if (!data || (data.currently_playing === null && !data.queue.length)) return <div>No active device or music playing.</div>;
 
   return (
-    <div className="spotify-player-container">
-      {/* Currently Playing Track */}
-      {data.currently_playing && (
-        <div className="currently-playing">
-          <h3>Now Playing:</h3>
-          <p>{data.currently_playing.name} - {data.currently_playing.artists[0]?.name}</p>
-        </div>
-      )}
-
-      {/* Up Next / Queue */}
-      <div className="queue-list">
-        <h3>Up Next:</h3>
-        {data.queue.length === 0 ? (
-          <p>Queue is empty.</p>
-        ) : (
-          <ul>
+    <Grid>
+      <Grid.Col span={9}>
+          {data.currently_playing && (
+            <SimpleGrid cols={2} py='lg' className={classes['spotify-container']}>
+              <Box>
+                <Image src={data.currently_playing.album.images[0].url} radius='lg' className={classes['now-playing-album']} />                
+              </Box>
+              <Box>
+                  <Stack justify='center' h={'100%'} gap='xs'>
+                    <Title order={4} className={classes['now-playing-title']} c={'gray'}>Now Playing</Title>
+                    <Title>{data.currently_playing.name}</Title>
+                    <Title order={3}>{data.currently_playing.artists[0]?.name}</Title>
+                    <Title order={4}>{data.currently_playing.album.name}</Title>
+                  </Stack>
+              </Box>
+            </SimpleGrid>
+          )}
+      </Grid.Col>
+      <Grid.Col span={3}>
+        <Title order={4}>Up Next</Title>
+          {data.queue.length === 0 ? (
+            <p>Queue is empty.</p>
+          ) : (
+          <ol>
             {data.queue.map((track: SimplifiedTrack, index: number) => (
-              <li key={`${track.id}-${index}`}>
-                <strong>{index + 1}.</strong> {track.name} by {track.artists[0]?.name}
-              </li>
+                <li key={`${track.id}-${index}`} className={classes['queue-item']}>
+                  <Group justify='center' className={classes['queue-item-container']}>
+                  <Image src={track.album.images[0].url} radius='lg' className={classes['queue-album-art']} />
+                  <Stack justify='center' gap='0' className={classes['queue-item-track-info']}>
+                    <Text truncate='end' flex={1}>{track.name}</Text>
+                    <Text truncate='end' flex={1}>{track.artists[0]?.name}</Text>
+                  </Stack>
+                  <Text>{formatTime(track.duration_ms)}</Text>
+                  </Group>
+                </li>
             ))}
-          </ul>
-        )}
-      </div>
-    </div>
+          </ol>)}
+      </Grid.Col>
+    </Grid>
   );
 }
 
--- src/web/react/pages/SongRequests.tsx
+++ src/web/react/pages/SongRequests.tsx
@@ -2,20 +2,17 @@ import { Box, Container, Text, Title } from '@mantine/core'
 import React from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import SpotifyQueue from '../components/spotifyqueue/SpotifyQueue';
-import { useSpotifyAuth } from '../lib/spotify'
-
 
 const SongRequests: React.FC = () => {
   const { isAuthenticated } = useAuth()
-  const { token } = useSpotifyAuth()
   return (
-    <Container>
+    <Container fluid>
       <Box id="heading">
           <Title>Song Requests</Title>
           <Text>{isAuthenticated ? 'Manage' : 'View'} Song Requests</Text>
       </Box>
 
-      <SpotifyQueue accessToken={token}/>
+      <SpotifyQueue />
     </Container>
   );
 };
