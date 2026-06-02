38eea72abe999a6868fdc40a4edfb8a03a13ea17
secondubly <secondubly@gmail.com>
2026-06-02 06:42:22 -0400

    fix typo

--- src/server/plugins/external/jwt.ts
+++ src/server/plugins/external/jwt.ts
@@ -4,7 +4,7 @@ import fjwt, { type FastifyJWTOptions } from '@fastify/jwt'
 const opts: FastifyJWTOptions = {
     secret:
         process.env.JWT_TOKEN ??
-        'This_Should_Be_A_Unique_Randomly_Geneated_String',
+        'This_Should_Be_A_Unique_Randomly_Generated_String',
 }
 
 export default fp(async (fastify) => {
