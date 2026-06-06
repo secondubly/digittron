14741604a522c9554e56b2e54165c65520150b47
secondubly <secondubly@gmail.com>
2026-06-05 23:59:08 -0400

    enforce typing of process.env so we can get rid of all the annoying variable enforcement

---
+++ src/config.ts
@@ -0,0 +1,40 @@
+import { log } from '@lib/services/logger'
+import { Type, type StaticDecode } from '@sinclair/typebox'
+import { Value } from '@sinclair/typebox/value'
+
+const SpaceSeparatedChannels = Type.Transform(Type.String({ minLength: 1 }))
+    .Decode((values) => {
+        const channels = values.split(' ').map((c) => c.trim())
+
+        return channels // string[]
+    })
+    .Encode((channels) => channels.join(','))
+
+const EnvSchema = Type.Object({
+    TWITCH_CLIENT_ID: Type.String({ minLength: 1 }),
+    TWITCH_ACCESS_TOKEN: Type.String({ minLength: 1 }),
+    TWITCH_BROADCASTER_ID: Type.String({ minLength: 1 }),
+    TWITCH_CHANNELS: SpaceSeparatedChannels,
+    // LEAD_TIME_MS: Type.Number({ default: 60_000 }),
+    // POLL_INTERVAL_MS: Type.Number({ default: 300_000 }),
+    NODE_ENV: Type.Union(
+        [Type.Literal('development'), Type.Literal('production')],
+        { default: 'production' },
+    ),
+})
+
+type Env = StaticDecode<typeof EnvSchema>
+
+const coerced = Value.Convert(EnvSchema, { ...process.env })
+const withDefaults = Value.Default(EnvSchema, coerced)
+
+if (!Value.Check(EnvSchema, withDefaults)) {
+    const errors = [...Value.Errors(EnvSchema, withDefaults)]
+
+    log.app.error('Invalid environment variables:')
+    // REVIEW: check this
+    errors.forEach(({ path, message }) => console.error(`   ${path}:`, message))
+    process.exit(1)
+}
+
+export const config = Value.Decode(EnvSchema, withDefaults) as Env
