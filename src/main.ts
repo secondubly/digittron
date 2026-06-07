da7c6207ad54723a5fa0797237bb373f06d99b5a
secondubly <secondubly@gmail.com>
2026-06-07 13:35:15 -0400

    rewrite index to main

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { init as apiInit } from './server/index.js'
+// import { init as webInit } from './web/index.js'
+import { init as botInit } from './lib/utils.js'
+import { log } from '@lib/services/logger.js'
+import { config } from './config.js'
+
+const main = async () => {
+    const server = await apiInit()
+    botInit(config.TWITCH_CHANNELS, server.tokenStore)
+    // webInit(parseInt(config.WEB_PORT))
+}
+
+main().catch((err) => {
+    log.app.error(`Failed to start app: `, err)
+})
