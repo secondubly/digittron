b266c2e8668110dd712f393870dc5be3b58ef5df
secondubly <secondubly@gmail.com>
2026-06-07 16:38:54 -0400

    move api helpers to dedicated folder

---
+++ tests/helpers/apiHelpers.ts
@@ -0,0 +1,34 @@
+import Fastify, { preHandlerAsyncHookHandler } from 'fastify'
+import type { MikroORM } from '@mikro-orm/core'
+import bootstrap from '../../src/server/build.js'
+import fp from 'fastify-plugin'
+import { TestContext } from 'node:test'
+
+declare module 'fastify' {
+    interface FastifyInstance {
+        authenticate: preHandlerAsyncHookHandler
+        orm: MikroORM
+    }
+}
+
+export function config() {
+    return {
+        skipOverride: true,
+    }
+}
+
+export async function build(t?: TestContext) {
+    const server = Fastify()
+    server.register(fp(bootstrap), config())
+
+    await server.ready()
+
+    if (t) {
+        t.after(() => {
+            server.orm.close()
+            server.close()
+        })
+    }
+
+    return server
+}
