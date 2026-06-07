0091fb2969dccc872d277f94cde6c18b4b58863f
secondubly <secondubly@gmail.com>
2026-06-07 13:38:12 -0400

    rewrite anniversary command

--- src/bot/commands/anniversary.ts
+++ src/bot/commands/anniversary.ts
@@ -1,13 +1,13 @@
-import type { Command, CommandContext } from '@lib/bot/types.js'
+import type { Command } from '@lib/bot/types.js'
 import { config } from 'src/config'
 
 const anniversary: Command = {
     name: 'anniversary',
     aliases: ['anni'],
     description: 'Anniversary stream inforamtion',
-    execute: async function (ctx: CommandContext): Promise<void> {
-        const broadcaster = ctx.channel
-        ctx.client.chat.sendChatMessageAsApp(
+    execute: async function ({ channel, client }): Promise<void> {
+        const broadcaster = channel
+        client.chat.sendChatMessageAsApp(
             config.TWITCH_BOT_ID,
             config.TWITCH_BROADCASTER_ID,
             `@${broadcaster}'s stream anniversary celebration is september 27 - september 30!
