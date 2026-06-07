25409da3e9c8766081397b04ed79a8b661ebd570
secondubly <secondubly@gmail.com>
2026-06-07 13:37:02 -0400

    rename bot index to main

---
+++ src/bot/main.ts
@@ -0,0 +1,39 @@
+import { log } from '@lib/services/logger'
+import { config } from 'src/config'
+
+import { createClient, type RedisClientType } from 'redis'
+import { TokenStore } from '@lib/core/tokens/TokenStore.js'
+import { MikroORM } from '@mikro-orm/sqlite'
+import mikroOrmConfig from 'src/mikro-orm.config'
+import { init } from '@lib/utils'
+
+async function createTokenStore() {
+    const redis = createClient({ url: config.REDIS_URL }) as RedisClientType
+
+    redis.on('error', (err) => log.app.error({ err }, 'Redis error'))
+    redis.on('connect', () => log.app.info('Redis connecting...'))
+    redis.on('ready', () => log.app.info('Redis ready'))
+    redis.on('reconnecting', () => log.app.warn('Redis reconnecting...'))
+
+    const orm = await MikroORM.init(mikroOrmConfig)
+
+    const tokenStore = new TokenStore(redis, orm)
+    tokenStore.connect()
+    return tokenStore
+}
+
+const startup = async () => {
+    if (config.NODE_ENV === 'development') {
+        log.bot.info('Running in development mode')
+    }
+
+    const tokenStore = await createTokenStore()
+    init(config.TWITCH_CHANNELS, tokenStore)
+}
+
+// only used when spinning up the bot by itself
+if (import.meta.main) {
+    startup().catch((err) => {
+        log.bot.error(`Failed to start bot: ${err}`)
+    })
+}
