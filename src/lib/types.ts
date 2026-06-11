9ae82826dbe848a96f49e02b6e166b35378e1a6c
secondubly <secondubly@gmail.com>
2026-06-11 19:41:05 -0400

    remove unused imports

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,3 @@
-import { ApiClient } from '@twurple/api'
-import type { ChatMessage } from '@twurple/chat'
-import { EventSubChannelChatMessageEvent } from '@twurple/eventsub-base'
-
 // REF: used for https://api.deadlock-api.com/docs
 export interface MMRHistory {
     account_id: number
