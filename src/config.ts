a97b82fcb060feff0f1c89dba6e0e77216a44cd2
secondubly <secondubly@gmail.com>
2026-06-11 20:47:30 -0400

    log config errors if found

--- src/config.ts
+++ src/config.ts
@@ -43,7 +43,7 @@ if (!Value.Check(EnvSchema, withDefaults)) {
 
     log.app.error('Invalid environment variables')
     // REVIEW: check this
-    // errors.forEach(({ path, message }) => console.error(`   ${path}:`, message))
+    errors.forEach(({ path, message }) => console.error(`   ${path}:`, message))
     process.exit(2)
 }
 
