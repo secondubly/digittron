03738562725ecadf53f4158d0bbc75fa8ba7831b
secondubly <secondubly@gmail.com>
2026-06-14 20:32:39 -0400

    fixed

--- src/bot/commands/rank.ts
+++ src/bot/commands/rank.ts
@@ -1,6 +1,17 @@
 import type { CommandContext, CommandDeps } from '@lib/bot/types'
-import type { MMRHistory } from '@lib/types.js'
-import { config } from 'src/config'
+import type { TokenStore } from '@lib/core/tokens/TokenStore'
+import { log } from '@lib/services/logger'
+import { config } from 'src/config/env'
+
+export interface MMRHistory {
+    account_id: number
+    match_id: number
+    start_time: number
+    player_score: number
+    rank: number
+    division: number
+    division_tier: number
+}
 
 const DEADLOCK_RANKS = new Map<number, string>([
     [0, 'Obscurus'],
@@ -22,62 +33,68 @@ const rank = ({ tokenStore }: CommandDeps) => ({
     aliases: [],
     description: 'Show Daedlock rank',
     async execute({ msg, client }: CommandContext) {
-        const { broadcasterId } = msg
-        const response = await fetch(
-            `https://api.deadlock-api.com/v1/players/mmr?account_ids=${config.STEAM_ID}`,
-            {
-                method: 'GET',
-            },
-        )
-
-        if (!response.ok) {
-            if (response.status === 500) {
-                // use cached value
-                const rank = tokenStore.get(`token:deadlock:${config.STEAM_ID}`)
-                if (!rank) {
-                    client.chat.sendChatMessageAsApp(
-                        config.TWITCH_BOT_ID,
-                        broadcasterId,
-                        'No rank found!',
-                    )
-                    return
-                }
-
-                client.chat.sendChatMessageAsApp(
-                    config.TWITCH_BOT_ID,
-                    broadcasterId,
-                    `Rank: ${rank}`,
-                )
-                return
-            } else {
-                client.chat.sendChatMessageAsApp(
-                    config.TWITCH_BOT_ID,
-                    broadcasterId,
-                    `Invalid user provided.`,
-                )
-                return
-            }
-        } else {
-            const data: MMRHistory[] = await response.json()
-            const firstResult = data[0]
-            const rank = DEADLOCK_RANKS.get(firstResult.division)
-            if (!rank) {
-                client.chat.sendChatMessageAsApp(
-                    process.env.BOT_ID!,
-                    broadcasterId,
-                    'No rank found!',
-                )
-                return
-            }
-
-            client.chat.sendChatMessageAsApp(
-                process.env.BOT_ID!,
-                broadcasterId,
-                `Rank: ${rank} ${firstResult.division_tier}`,
+        if (!config.STEAM_ID) {
+            log.bot.warn(
+                `${msg.chatterName} tried to execute !${this.name} but steam id is not configured, exiting early...`,
             )
             return
         }
+
+        const { broadcasterId } = msg
+        const deadlockData = await getDeadlockRank(config.STEAM_ID, tokenStore)
+
+        if (!deadlockData) {
+            client.chat.sendChatMessageAsApp(
+                config.TWITCH_BOT_ID,
+                broadcasterId,
+                'No rank found!',
+            )
+
+            return
+        }
+
+        tokenStore.set(`deadlock:${config.STEAM_ID}`, deadlockData)
+        const { division, division_tier } = deadlockData
+        const namedRank = DEADLOCK_RANKS.get(division)
+        client.chat.sendChatMessageAsApp(
+            config.TWITCH_BOT_ID,
+            broadcasterId,
+            `Rank: ${namedRank} (Division: ${division_tier})`,
+        )
     },
 })
+
+async function getDeadlockRank(
+    steamId: string,
+    tokenStore: TokenStore,
+): Promise<MMRHistory | null> {
+    const response = await fetch(
+        `https://api.deadlock-api.com/v1/players/mmr?account_ids=${steamId}`,
+        {
+            method: 'GET',
+        },
+    )
+
+    let rank: MMRHistory | null
+    if (!response.ok) {
+        if (response.status === 500) {
+            // use cached value
+            rank = (await tokenStore.get(
+                `deadlock:${steamId}`,
+            )) as unknown as MMRHistory
+            if (!rank) return null
+        } else {
+            // any other error just return null
+            return null
+        }
+    } else {
+        const data: MMRHistory[] = await response.json()
+        const firstResult = data[0]
+
+        rank = firstResult
+    }
+
+    return rank
+}
 
 export default rank
