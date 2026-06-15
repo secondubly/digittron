07426f60fa2aec06bb8b3705e09413dc553cf3fd
secondubly <secondubly@gmail.com>
2026-06-15 00:22:00 -0400

    set message tracker online if stream goes online while bot is up

--- src/bot/events/eventsub/onStreamOnline.ts
+++ src/bot/events/eventsub/onStreamOnline.ts
@@ -1,13 +1,14 @@
 import { log } from '@lib/services/logger'
 import type { BotContext, EventDeps } from '../types'
 
-export default ({ bot }: EventDeps) => ({
+export default ({ bot, firstMessageTracker }: EventDeps) => ({
     type: 'eventsub',
     name: 'onStreamOnline',
     register({ eventSub, broadcasterId }: BotContext) {
         eventSub.onStreamOnline(broadcasterId, (_event) => {
-            bot.startAdPoller()
             log.bot.debug(`Received stream online event for ${broadcasterId}`)
+            firstMessageTracker.setOnline()
+            bot.startAdPoller()
         })
     },
 })
