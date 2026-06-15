49b8519c5be532847859969fe7b408aae99108c0
secondubly <secondubly@gmail.com>
2026-06-14 20:40:59 -0400

    works right out the box 😃

--- src/bot/commands/playlist.ts
+++ src/bot/commands/playlist.ts
@@ -1,14 +1,14 @@
 import type { Command, CommandContext } from '@lib/bot/types.js'
+import { config } from 'src/config/env'
 
 const playlist: Command = {
     name: 'playlist',
     aliases: [],
-    enabled: true,
     description: 'Get the streamer’s playlist',
     async execute({ msg, client }: CommandContext) {
         const { broadcasterId, chatterDisplayName } = msg
         client.chat.sendChatMessageAsApp(
-            process.env.BOT_ID!,
+            config.TWITCH_BOT_ID,
             broadcasterId,
             `@${chatterDisplayName} starting soon playlist: https://open.spotify.com/playlist/666mjTAcb7PYiWEnincpNt?si=54d75be2632741ac`,
         )
