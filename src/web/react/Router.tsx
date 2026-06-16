107ac68323055f456f7b55d191b2b72b4305976d
secondubly <secondubly@gmail.com>
2026-06-15 21:04:21 -0400

    rename overlaypage to prevent confusion

--- src/web/react/Router.tsx
+++ src/web/react/Router.tsx
@@ -2,13 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Commands from './pages/Commands'
-import Register from './pages/Register'
 import ChatModeration from './pages/ChatModeration'
 import SongRequests from './pages/SongRequests'
 import SpotifyLogin from './pages/SpotifyLogin'
 import TwitchLogin from './pages/TwitchLogin'
 import ProtectedRoute from "./components/protected_routes/ProtectedRoutes";
 import Giveaways from "./pages/Giveaways";
+import { OverlayPage } from "./pages/Overlay";
 
 const AppRoutes = () => {
     return (
@@ -17,7 +17,6 @@ const AppRoutes = () => {
             <Route path="/commands" element={
                 <Commands />
             } />
-            <Route path="/register" element={<Register />} />
             <Route path="/giveaways" element={
                 <ProtectedRoute>
                     <Giveaways />
@@ -40,6 +39,11 @@ const AppRoutes = () => {
             <Route path="/twitch_login" element={
                 <TwitchLogin />
              } />
+
+             <Route path="/overlay" element={
+                <OverlayPage />
+             } />
+
             <Route path="*" element={<NotFound />} />
         </Routes>
     )
