0cf12b95cff43fdfd8d19513561d6984817a540a
secondubly <secondubly@gmail.com>
2026-06-16 23:13:22 -0400

    feat:update start file

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,29 @@
-import { init as apiInit } from './server/index.js'
-// import { init as webInit } from './web/index.js'
-import { init as botInit } from './lib/utils.js'
+import { init as buildServer } from './server/startup.js'
+import { init as webInit } from './web/index.js'
 import { log } from '@lib/services/logger.js'
 import { config } from './config/env.js'
 
+// server controls bot as well
 const main = async () => {
-    const server = await apiInit()
-    botInit(config.TWITCH_CHANNELS, server.tokenStore)
-    // webInit(parseInt(config.WEB_PORT))
+    const server = await buildServer()
+    try {
+        if (process.stdout.isTTY || config.NODE_ENV === 'development') {
+            server.log.info('Server running in development mode')
+            server.listen({
+                port: (process.env.API_PORT as unknown as number) ?? 4001,
+            })
+            // start web server if we're in dev mode
+            webInit((process.env.WEB_PORT as unknown as number) ?? 50001)
+        } else {
+            server.listen({
+                port: (process.env.API_PORT as unknown as number) ?? 4001,
+                host: '0.0.0.0',
+            })
+        }
+    } catch (err) {
+        server.log.error(err)
+        process.exit(1)
+    }
 }
 
 main().catch((err) => {
