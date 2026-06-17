8af02418e5341992691e5d0e311101d090a6b955
secondubly <secondubly@gmail.com>
2026-06-16 23:12:21 -0400

    feat(bot): add startup messages ONLY on first connection

--- src/bot/events/chat/onAuthenticationSuccess.ts
+++ src/bot/events/chat/onAuthenticationSuccess.ts
@@ -1,12 +1,33 @@
 import { log } from '@lib/services/logger'
-import type { ChatEvent } from '../types'
+import type { EventDeps, EventSubEvent } from '../types'
+import { config } from 'src/config/env'
 
-export default {
-    type: 'chat',
+let isFirstConnection = true
+
+export default ({ say }: EventDeps): EventSubEvent => ({
+    type: 'eventsub',
     name: 'onAuthenticationSuccess',
     register({ chatClient }) {
         chatClient.onAuthenticationSuccess(() => {
             log.bot.info(`Ready to yap! 😃`)
+            const startupMessages = [
+                'I’m here, so we‘re good.',
+                'Sorry, fell asleep there for a second.',
+                'Safe journeys. Straight aim. And good huntin‘.',
+                'Psst, hey. Take me with you. I hate this job.',
+            ]
+
+            if (isFirstConnection) {
+                const message =
+                    startupMessages[
+                        Math.floor(Math.random() * startupMessages.length)
+                    ]
+                config.TWITCH_CHANNELS.forEach((chan) => say(chan, message))
+                isFirstConnection = false
+            }
+            log.bot.info(
+                `${isFirstConnection ? 'Connected' : 'Reconnected'} to Twitch, requesting ${config.TWITCH_CHANNELS.length} channels: ${config.TWITCH_CHANNELS.join(', ')}`,
+            )
         })
     },
-} satisfies ChatEvent
+})
