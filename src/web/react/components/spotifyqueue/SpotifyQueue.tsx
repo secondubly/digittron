3f635a3e04286f50627883f0078b2ecedece1071
secondubly <secondubly@gmail.com>
2026-06-21 11:24:09 -0400

    feat(web): spotify queue uses separate queue and current track fetchers

--- src/web/react/components/spotifyqueue/SpotifyQueue.tsx
+++ src/web/react/components/spotifyqueue/SpotifyQueue.tsx
@@ -1,9 +1,9 @@
 import classes from './SpotifyQueue.module.css'
 import { type SimplifiedTrack } from '../../types/spotify'
-import { ActionIcon, Box, Flex, Group, Image, ScrollArea, Stack, Text, Title, Divider, Paper } from '@mantine/core'
+import { ActionIcon, Box, Flex, Group, Image, ScrollArea, Stack, Text, Title, Divider, Paper, Center, Loader } from '@mantine/core'
 import { IconMusicPlus } from '@tabler/icons-react'
 import { MarqueeText } from '../MarqueeText/MarqueeText'
-import { usePlayback } from '../../hooks/useSpotify'
+import { usePlayback, useQueue } from '../../hooks/useSpotify'
 
 
 const formatTime = (ms: number) => {
@@ -12,35 +12,55 @@ const formatTime = (ms: number) => {
 
 const SpotifyQueue = () => {
   const { playback: data, isLoading, error } = usePlayback()
+  const { queue } = useQueue()
 
-  if (isLoading) return <div>Loading player queue...</div>;
+  if (isLoading) {
+    return (
+      <Center h={200}>
+        <Loader size="sm" />
+      </Center>
+    )
+  }
   if (error) return <div>Failed to load queue. Ensure Spotify is active.</div>;
 
-  if (!data || data.device.is_private_session || (data.currently_playing === null && !data.queue.length)) return <div>No active device or music playing.</div>;
+  if (!data || (data.currently_playing === null && !queue.length)) {
+    return (
+      <Center h={200}>
+        <Stack align="center" gap="xs">
+          <Text fw={500}>No active playback</Text>
+          <Text size="sm" c="dimmed">
+            Open Spotify on any device to get started
+          </Text>
+        </Stack>
+      </Center>
+    )
+  }
 
+  const track = data?.item
+  
   return (
           <Box>
-            <Flex gap={'xl'} visibleFrom='md'>
+            <Flex gap={'10em'} visibleFrom='md'>
               <Group className={classes['now-playing-container']}>  
-                  <Image src={data.currently_playing.album.images[0].url} radius='lg' className={classes['now-playing-album']} />                
+                  <Image src={track.album.images[0].url} radius='lg' className={classes['now-playing-album']} />                
                   <Stack justify='center' h={'100%'} gap='xs'>
                     <Title order={4} className={classes['now-playing-title']} c={'gray'}>Now Playing</Title>
-                    <Title>{data.currently_playing.name}</Title>
-                    <Title order={3}>{data.currently_playing.artists[0]?.name}</Title>
-                    <Title order={4}>{data.currently_playing.album.name}</Title>
+                    <Title>{track.name}</Title>
+                    <Title order={3}>{track.artists[0]?.name}</Title>
+                    <Title order={4}>{track.album.name}</Title>
                   </Stack>
               </Group>
               <Box className={classes['queue-container']}>
                 <Box pl='md' pb='lg' style={{ 'borderBottom': '1px solid var(--mantine-color-light-dark(dark-3,light.3))'}}>
                   <Title order={4}>Up Next</Title>
-                  <Text c={'dark.2'}>{data.queue.length} songs in queue</Text>
+                  <Text c={'dark.2'}>{queue.length} songs in queue</Text>
                 </Box>
-                {data.queue.length === 0 ? (
+                {queue.length === 0 ? (
                   <p>Queue is empty.</p>
                 ) : (
                 <ScrollArea h={'77.5vh'} type='auto' offsetScrollbars my={'md'} overscrollBehavior='contain'>
                   <Box component='ol' className='queue-list'>
-                    {data.queue.map((track: SimplifiedTrack, index: number) => (
+                    {queue.map((track: SimplifiedTrack, index: number) => (
                         <li key={`${track.id}-${index}`} className={classes['queue-item']}>
                           <Image src={track.album.images[0].url} radius='md' className={classes['queue-album-art']} />
                           <Stack gap='0' className={classes['queue-item-track-info']}>
@@ -59,15 +79,15 @@ const SpotifyQueue = () => {
             <Box hiddenFrom='md'>
               <Stack>
                 <Box>
-                  <Image src={data.currently_playing.album.images[0].url} className={classes['now-playing-album']} w='100%' />
+                  <Image src={track.album.images[0].url} className={classes['now-playing-album']} w='100%' />
                 </Box>
                 <Box>
                   <Stack gap={0}>
                     <Group wrap='nowrap' justify='space-between'>
-                      <MarqueeText text={data.currently_playing.name} pauseOnHover={false} speed={30} />
+                      <MarqueeText text={track.name} pauseOnHover={false} speed={30} />
                       <ActionIcon bg={'transparent'} size='xl'><IconMusicPlus size='100%' /></ActionIcon>
                     </Group>
-                    <Text size='lg'>{data.currently_playing.artists[0]?.name}</Text>
+                    <Text size='lg'>{track.artists[0]?.name}</Text>
                   </Stack>
                 </Box>
               </Stack>
@@ -76,7 +96,7 @@ const SpotifyQueue = () => {
                 <Title order={4} fw={'600'}>Up Next</Title>
                 <Paper p={'xs'}>
                   <Box component='ul' className='queue-list' p={0} mt={0}>
-                    {data.queue.map((track: SimplifiedTrack, index: number) => (
+                    {queue.map((track: SimplifiedTrack, index: number) => (
                         <li key={`${track.id}-${index}`} className={classes['queue-item']}>
                           <Image src={track.album.images[0].url} radius='md' className={classes['queue-album-art']} />
                           <Stack gap='0' className={classes['queue-item-track-info']}>
--- src/web/react/hooks/useSpotify.ts
+++ src/web/react/hooks/useSpotify.ts
@@ -4,13 +4,27 @@ export const usePlayback = () => {
     // SWR constructor: (url endpoint (aka key), fetch HTTP request to call on the endpoint, {optional parameters})
     const { data, error, mutate } = useSWR('/me/player', spotifyFetcher, {
         // refresh every 2 seconds if a song is playing; otherwise, refresh immediately
-        refreshInterval: (data) => (data?.is_playing ? 2000 : 0),
+        refreshInterval: (data) => (data?.currently_playing ? 2000 : 0),
         dedupingInterval: 1000,
         revalidateOnFocus: false,
     })
 
     return {
         playback: data,
+        isLoading: !data && !error,
+        error,
+        mutate,
+    }
+}
+
+export const useQueue = () => {
+    const { data, error, mutate } = useSWR('/me/player/queue', spotifyFetcher, {
+        refreshInterval: 10000,
+        revalidateOnFocus: false,
+    })
+
+    return {
+        queue: data?.queue ?? [],
         isLoading: !data && !error,
         error,
         mutate,
