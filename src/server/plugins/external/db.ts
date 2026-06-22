ad06d067451e735a1e47f281c4dfa7e770ae75d7
secondubly <secondubly@gmail.com>
2026-06-22 17:47:12 -0400

    feat: complete any db migrations during startup

--- src/server/plugins/external/db.ts
+++ src/server/plugins/external/db.ts
@@ -3,10 +3,14 @@ import type { FastifyInstance } from 'fastify'
 import { MikroORM } from '@mikro-orm/core'
 import type { EntityManager } from '@mikro-orm/sqlite'
 import mikroOrmConfig from '../../../mikro-orm.config'
+import { log } from '@core/utils/logger'
 
 export default fp(
   async (server: FastifyInstance) => {
     const orm = await MikroORM.init(mikroOrmConfig)
+
+    log.app.info('Checking for database migrations...')
+    await orm.migrator.up()
 
     server.decorate('orm', orm)
     server.decorate('db', orm.em)
