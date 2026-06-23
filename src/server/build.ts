9ade2b9d60259251167aae16871ea72abc026815
secondubly <secondubly@gmail.com>
2026-06-23 09:33:24 -0400

    style: formatting

--- src/server/build.ts
+++ src/server/build.ts
@@ -1,80 +1,46 @@
 import path from 'node:path'
 import fastifyAutoload from '@fastify/autoload'
-import type { FastifyError, FastifyInstance, FastifyPluginOptions } from 'fastify'
+import type {
+  FastifyError,
+  FastifyInstance,
+  FastifyPluginAsync,
+  FastifyPluginOptions,
+} from 'fastify'
 
-export default async function bootstrap(fastify: FastifyInstance, opts: FastifyPluginOptions) {
-  delete opts.skipOverride
-  /**
-   * load external plugins first because the server may need them immediately
-   */
-  await fastify
-    .register(fastifyAutoload, {
-      dir: path.join(import.meta.dirname, 'plugins/external'),
-      options: { ...opts },
-      encapsulate: false,
-      matchFilter: (pathToFile) => {
-        if (pathToFile.endsWith('bot.ts') && !fastify.withBot) {
-          return false
-        }
-        return true
-      },
-    })
-    .after((err) => {
-      if (err) throw err
-      fastify.log.debug('All autoloaded plugins are loaded and ready!')
-    })
-
-  fastify.register(fastifyAutoload, {
-    dir: path.join(import.meta.dirname, 'routes'),
-    autoHooks: true,
-    cascadeHooks: true,
-    options: { ...opts },
-  })
-
-  fastify.setErrorHandler((err: FastifyError, request, reply) => {
-    fastify.log.error(
-      {
-        err,
-        request: {
-          method: request.method,
-          url: request.url,
-          query: request.query,
-          params: request.params,
+export default async function bootstrap(opts: FastifyPluginOptions): Promise<FastifyPluginAsync> {
+  return async (fastify: FastifyInstance) => {
+    delete opts.skipOverride
+    /**
+     * load external plugins first because the server may need them immediately
+     */
+    await fastify
+      .register(fastifyAutoload, {
+        dir: path.join(import.meta.dirname, 'plugins/external'),
+        options: { ...opts },
+        encapsulate: false,
+        matchFilter: (pathToFile) => {
+          if (pathToFile.endsWith('bot.ts') && !fastify.withBot) {
+            return false
+          }
+          return true
         },
-      },
-      'Unhandled error occurred',
-    )
+      })
+      .after((err) => {
+        if (err) throw err
+        fastify.log.debug('All autoloaded plugins are loaded and ready!')
+      })
 
-    reply.code(err.statusCode ?? 500)
-    let message = 'Internal Server Error'
-    if (err.statusCode && err.statusCode < 500) {
-      message = err.message
-    }
+    fastify.register(fastifyAutoload, {
+      dir: path.join(import.meta.dirname, 'routes'),
+      autoHooks: true,
+      cascadeHooks: true,
+      options: { ...opts },
+    })
 
-    return { message }
-  })
-
-  // An attacker could search for valid URLs if error handling is not rate limited.
-  fastify.setNotFoundHandler(
-    {
-      preHandler: fastify.rateLimit({
-        max: 1,
-        timeWindow: 500,
-      }),
-    },
-    (request, reply) => {
-      const knownPathPrefixes = ['/api/spotify/token', '/api/twitch/token']
-      const matchesPartialPath = knownPathPrefixes.some((p) => request.url.startsWith(p))
-
-      if (matchesPartialPath && request.method === 'GET') {
-        return reply.code(400).send({
-          statusCode: 400,
-          error: 'Bad Request',
-          message: 'Missing required path parameters',
-        })
-      }
-      request.log.warn(
+    fastify.setErrorHandler((err: FastifyError, request, reply) => {
+      fastify.log.error(
         {
+          err,
           request: {
             method: request.method,
             url: request.url,
@@ -82,18 +48,57 @@ export default async function bootstrap(fastify: FastifyInstance, opts: FastifyP
             params: request.params,
           },
         },
-        'Resource not found, redirecting to index.html',
+        'Unhandled error occurred',
       )
 
-      // return reply.code(404).send({
-      //   statusCode: 404,
-      //   error: 'Not Found',
-      //   message: `Route ${request.method}:${request.url} not found`,
-      // })
+      reply.code(err.statusCode ?? 500)
+      let message = 'Internal Server Error'
+      if (err.statusCode && err.statusCode < 500) {
+        message = err.message
+      }
 
-      reply.sendFile('index.html')
-    },
-  )
+      return { message }
+    })
 
-  console.log('bootstrap end')
+    // An attacker could search for valid URLs if error handling is not rate limited.
+    fastify.setNotFoundHandler(
+      {
+        preHandler: fastify.rateLimit({
+          max: 1,
+          timeWindow: 500,
+        }),
+      },
+      (request, reply) => {
+        const knownPathPrefixes = ['/api/spotify/token', '/api/twitch/token']
+        const matchesPartialPath = knownPathPrefixes.some((p) => request.url.startsWith(p))
+
+        if (matchesPartialPath && request.method === 'GET') {
+          return reply.code(400).send({
+            statusCode: 400,
+            error: 'Bad Request',
+            message: 'Missing required path parameters',
+          })
+        }
+        request.log.warn(
+          {
+            request: {
+              method: request.method,
+              url: request.url,
+              query: request.query,
+              params: request.params,
+            },
+          },
+          'Resource not found, redirecting to index.html',
+        )
+
+        // return reply.code(404).send({
+        //   statusCode: 404,
+        //   error: 'Not Found',
+        //   message: `Route ${request.method}:${request.url} not found`,
+        // })
+
+        reply.sendFile('index.html')
+      },
+    )
+  }
 }
