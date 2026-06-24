b9da572d184ea645b56b1a871392d264fdb6e4d1
secondubly <secondubly@gmail.com>
2026-06-23 22:47:53 -0400

    fix: use proper firstMessageTracker

--- src/bot/events/eventsub/onStreamOnline.ts
+++ src/bot/events/eventsub/onStreamOnline.ts
@@ -1,10 +1,10 @@
 import { log } from '@core/utils/logger'
 import type { BotContext, EventDeps } from '../types'
 
-export default ({ bot, firstMessageTracker }: EventDeps) => ({
+export default ({ bot }: EventDeps) => ({
   type: 'eventsub',
   name: 'onStreamOnline',
-  register({ eventSub, broadcasterId }: BotContext) {
+  register({ eventSub, broadcasterId, firstMessageTracker }: BotContext) {
     eventSub.onStreamOnline(broadcasterId, (_event) => {
       log.bot.debug(`Received stream online event for ${broadcasterId}`)
       firstMessageTracker.setOnline()
