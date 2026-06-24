00a28ff02e730a3cf224113b04073dc2823fa23c
secondubly <secondubly@gmail.com>
2026-06-23 21:12:58 -0400

    fix: reference correct message tracker

--- src/bot/events/eventsub/onStreamOffline.ts
+++ src/bot/events/eventsub/onStreamOffline.ts
@@ -1,14 +1,12 @@
 import { log } from '@core/utils/logger'
 import type { BotContext, EventDeps } from '../types'
 
-export default ({ bot, firstMessageTracker }: EventDeps) => ({
+export default ({ bot }: EventDeps) => ({
   type: 'eventsub',
   name: 'onStreamOffline',
-  register({ eventSub, broadcasterId }: BotContext) {
+  register({ eventSub, broadcasterId, firstMessageTracker }: BotContext) {
     eventSub.onStreamOffline(broadcasterId, (event) => {
-      log.bot.debug(
-        `Stream offline, stopping services for ${event.broadcasterId}`,
-      )
+      log.bot.debug(`Stream offline, stopping services for ${event.broadcasterId}`)
 
       firstMessageTracker.setOffline()
       bot.stopAdPoller()
