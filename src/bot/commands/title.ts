b6930c5b27b443660f800086f9eb582da1fc7dbd
secondubly <secondubly@gmail.com>
2026-06-24 07:35:19 -0400

    fix: join args with space separator

--- src/bot/commands/title.ts
+++ src/bot/commands/title.ts
@@ -3,8 +3,7 @@ import type { Command, CommandContext } from '../types.js'
 const title: Command = {
   name: 'title',
   aliases: [],
-  description:
-    'Show stream title (for viwers) or change stream title (for moderators and up)',
+  description: 'Show stream title (for viwers) or change stream title (for moderators and up)',
   async execute({ msg, args, client, say }: CommandContext) {
     const { broadcasterId, chatterId, chatterDisplayName } = msg
     const channelInfo = await client.channels.getChannelInfoById(broadcasterId)
@@ -15,16 +14,13 @@ const title: Command = {
     if (!args.length) {
       say(`@${chatterDisplayName}, title: ${channelInfo.title}`)
     } else {
-      const isMod = await client.moderation.checkUserMod(
-        broadcasterId,
-        chatterId,
-      )
+      const isMod = await client.moderation.checkUserMod(broadcasterId, chatterId)
       const isBroadcaster = chatterId === process.env.TWITCH_ID
       if (!isMod && !isBroadcaster) {
         return
       }
 
-      const streamTitle = args.join()
+      const streamTitle = args.join(' ')
 
       client.channels.updateChannelInfo(broadcasterId, {
         title: streamTitle,
