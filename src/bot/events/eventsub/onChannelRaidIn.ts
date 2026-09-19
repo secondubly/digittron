e818c5fd3ff2df166fdc9115f4c68c81a9cd451b
secondubly <secondubly@gmail.com>
2026-09-19 11:04:27 -0400

    feature: add proper error handling for shoutouts

--- src/bot/events/eventsub/onChannelRaidIn.ts
+++ src/bot/events/eventsub/onChannelRaidIn.ts
@@ -1,33 +1,40 @@
 import { log } from '@core/utils/logger'
 import type { EventDeps, EventSubEvent } from '../types'
+import { error } from 'node:console'
 
 export default ({ apiClient, say }: EventDeps): EventSubEvent => ({
   type: 'eventsub',
   name: 'onChannelRaidIn',
-  register({ eventSub, broadcasterId, botUserId }) {
-    eventSub.onChannelRaidTo(broadcasterId, async (event) => {
+  async register({ eventSub, broadcasterId, botUserId }) {
+    const listener = eventSub.onChannelRaidTo(broadcasterId, async (event) => {
       // get game info for raidingUser
-      const channelInfo = await apiClient.channels.getChannelInfoById(
-        event.raidingBroadcasterId,
-      )
+      const raiderId = event.raidingBroadcasterId
+      const channelInfo = await apiClient.channels.getChannelInfoById(raiderId)
+
       if (!channelInfo) {
-        log.bot.warn('Could not retrieve channel info for raider')
+        log.bot.warn('Could not retrieve info for raiding broadcaster')
         return
       }
 
-      const gameInfo = await channelInfo.getGame()
-      if (!gameInfo) {
-        log.bot.warn('Could not get game info for raider')
-        return
-      }
+      const raidMsg = `Everyone say hi to ${event.raidingBroadcasterDisplayName}! They were playing ${channelInfo.gameName ?? 'absolutely nothing!'}!`
+      await say(event.raidedBroadcasterName, raidMsg)
 
       // shoutout raider
-      await apiClient.asUser(botUserId, async (ctx) => {
-        await ctx.chat.shoutoutUser(botUserId, event.raidingBroadcasterId)
-      })
-
-      const raidMsg = `Everyone say hi to ${event.raidingBroadcasterDisplayName}! They were playing ${gameInfo.name}!`
-      say(event.raidedBroadcasterName, raidMsg)
+      try {
+        await apiClient.asUser(botUserId, async (ctx) => {
+          await ctx.chat.shoutoutUser(broadcasterId, raiderId)
+        })
+      } catch (e) {
+        if (e instanceof Error) {
+          log.bot.error(`Error: ${e.message}`)
+        } else {
+          log.bot.error(`Unexpected error occurred: ${e}`)
+        }
+      }
     })
+
+    if (process.env.NODE_ENV !== 'production') {
+      console.log(await listener.getCliTestCommand())
+    }
   },
 })
