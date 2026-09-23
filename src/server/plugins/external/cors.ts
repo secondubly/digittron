21685ab303e68ddeb4b5fa9a21782955833fbda9
secondubly <secondubly@gmail.com>
2026-09-23 10:05:46 -0700

    refactor: update CORS handling to allow local network devices

--- src/server/plugins/external/cors.ts
+++ src/server/plugins/external/cors.ts
@@ -1,18 +1,26 @@
 import cors, { type FastifyCorsOptions } from '@fastify/cors'
+import type { FastifyInstance } from 'fastify'
 
 export const autoConfig: FastifyCorsOptions = {
-  origin: (origin, cb) => {
-    if (
-      !origin ||
-      /localhost:5000/.test(origin) ||
-      /localhost:5001/.test(origin) ||
-      /localhost:4000/.test(origin)
-    ) {
+  origin: function (this: FastifyInstance, origin, cb) {
+    this.log.info({ origin }, '[CORS] routing request')
+    // Allow requests with no origin (e.g. Postman, mobile apps, curl)
+    if (!origin) {
+      this.log.info('[CORS] No origin found')
       cb(null, true)
       return
     }
 
-    cb(new Error('Not allowed'), false)
+    const hostname = new URL(origin).hostname
+    const isLocal = hostname.startsWith('localhost') || hostname.startsWith('127.0.0.1') || hostname.startsWith('192.168.')
+
+    this.log.info(`[CORS] HOSTNAME: ${hostname}`)
+    if (isLocal) {
+      cb(null, true)
+      return
+    } else {
+      cb(new Error('[CORS] Not allowed'), false)
+    }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
