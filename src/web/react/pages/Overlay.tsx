8ca0e4b48cf6267045479f3f88b3a014f80ca062
secondubly <secondubly@gmail.com>
2026-09-23 19:53:58 -0700

    feat: update overlay page to use transparent background

--- src/web/react/pages/Overlay.tsx
+++ src/web/react/pages/Overlay.tsx
@@ -1,44 +1,4 @@
-// import { useCallback, useState } from "react";
-// import { useBotAudio } from '../hooks/useBotAudio'
-// import { useSSE } from '../hooks/useSSE'
-// import { CustomAudioManager } from "../components/custom_audio_manager/CustomAudioManager";
-// import { Stack, Switch } from "@mantine/core";
-
-// export function OverlayPage() {
-// const [muted, setMuted] = useState(false);
-
-//   const {
-//     playFirstMessage,
-//     customAudio,
-//   } = useBotAudio({
-//     muted,
-//   });
-
-//   useSSE({
-//     handlers: {
-//       firstMessage:  useCallback((data) => playFirstMessage(data), [playFirstMessage]),
-//     },
-//   });
-
-//   return (
-//     <Stack p="xl" gap="lg">
-
-//       {/* Global mute toggle */}
-//       <Switch
-//         label="Mute all sounds"
-//         checked={muted}
-//         onChange={e => setMuted(e.currentTarget.checked)}
-//         color="violet"
-//       />
-
-//       {/* Per-user audio management */}
-//       <CustomAudioManager customAudio={customAudio} />
-
-//     </Stack>
-//   );
-// }
-
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useSSE } from '../hooks/useSSE'
 import { useCustomAudio } from '../hooks/useCustomAudio'
 
@@ -50,6 +10,16 @@ interface FirstMessageEvent {
 }
 
 export function OverlayPage() {
+  useEffect(() => {
+    const originalBg = document.body.style.backgroundColor;
+    document.body.style.backgroundColor = 'transparent';
+
+    // Revert back to the original color when leaving the page
+    return () => {
+      document.body.style.backgroundColor = originalBg;
+    };
+  }, []);
+
   const { playForChatter } = useCustomAudio('/audio/first-message.mp3')
 
   useSSE({
